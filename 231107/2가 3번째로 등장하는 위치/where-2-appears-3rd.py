a33417e21b3d625ff99f925a7d1b7ec5088d6984
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-08 08:54:30 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 130ms, Memory: 73MB, Status: Passed - CodeTree

--- 231107/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 231107/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -2,7 +2,9 @@ n = int(input())
 arr = list(map(int, input().split()))
 cnt = 0
 for i in range(n) :
-    if arr[i] == 2 and cnt == 2 :
-        print(i+1)
-    else :
-        cnt += 1
+    if arr[i] == 2 :
+        if cnt == 2 :
+            print(i+1)
+            break
+        else :
+            cnt += 1
