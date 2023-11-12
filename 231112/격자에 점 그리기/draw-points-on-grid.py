d666897b7bf49bac18887b6927cf794a28f87824
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-12 23:22:47 +0900

    Title: 격자에 점 그리기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231112/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,16 @@
+n, m = tuple(map(int, input().split()))
+
+arr_2d = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+cnt = 1
+for _ in range(m) :
+    r,c = tuple(map(int, input().split()))
+    arr_2d[r-1][c-1] = cnt
+    cnt += 1
+
+for row in arr_2d :
+    for elem in row :
+        print(elem, end = " ")
+    print()
