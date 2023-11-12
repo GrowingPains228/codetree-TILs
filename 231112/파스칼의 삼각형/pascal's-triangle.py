340a3cac34d5a2135705dace1b3b737e4d77c346
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-12 22:07:46 +0900

    Title: 파스칼의 삼각형, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231112/파스칼의 삼각형/pascal's-triangle.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+arr_triangle = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+# 1열 초기화
+for i in range(n) :
+    arr_triangle[i][0] = 1
+
+for i in range(n) :
+    for j in range(i+1) :
+        arr_triangle[i][j] = arr_triangle[i-1][j-1] + arr_triangle[i-1][j]
+
+for i in range(n) :
+    for j in range(i+1) : 
+        print(arr_triangle[i][j], end = " ")
+    print()
