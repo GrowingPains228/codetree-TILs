e977b71fd54439fa62c3b2f058a116a4f542b0a5
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-14 08:23:25 +0900

    Title: 특정 문자의 유무, Time: 196ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231114/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,35 @@
+in_ee = "ee"
+in_ab = "ab"
+
+arr = input()
+
+# 1. 가장 쉬운 in 키워드를 써서 비교하는 방법
+#if in_ee in arr :
+#    print("Yes", end = " ")
+#else :
+#    print("No")
+
+#if in_ab in arr :
+#    print("Yes", end = " ")
+#else :
+#    print("No")
+
+#2. 길이만큼 전부 비교하기
+len_ee = len("ee")
+len_ab = len("ab")
+
+exits_tf = False
+for i in range(0, len(arr)-len_ee) :
+    if in_ee == arr[i:i+len_ee] :
+        exits_tf = True
+        break
+    
+print("Yes" if exits_tf else "No", end = " ")
+
+exits_tf = False
+for i in range(0, len(arr)-len_ab) :
+    if in_ab == arr[i:i+len_ab] :
+        exits_tf = True
+        break
+    
+print("Yes" if exits_tf else "No")
