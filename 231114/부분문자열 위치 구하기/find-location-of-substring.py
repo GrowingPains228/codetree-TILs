39551027e5b5a893affe7a20f16332e32a85597b
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-14 09:07:56 +0900

    Title: 부분문자열 위치 구하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231114/부분문자열 위치 구하기/find-location-of-substring.py
+++ 231114/부분문자열 위치 구하기/find-location-of-substring.py
@@ -1,5 +1,17 @@
 string = input()
 goal_string = input()
 
+#1. 쉬운 방법 (find keyword로 바로 해결)
+#print(string.find(goal_string))
 
-print(string.find(goal_string))
+#2.  for loop 돌면서 비교하기
+leng = len(string)
+leng_goal = len(goal_string)
+index = -1
+
+for i in range(leng -leng_goal + 1) :
+    if goal_string == string[i:i + leng_goal] :
+        index = i
+        break
+
+print(index)
