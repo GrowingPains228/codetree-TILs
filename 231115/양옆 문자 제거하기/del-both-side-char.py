9d53f10cd67285f679777bf394070c132d075d26
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-15 08:03:13 +0900

    Title: 양옆 문자 제거하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231115/양옆 문자 제거하기/del-both-side-char.py
+++ 231115/양옆 문자 제거하기/del-both-side-char.py
@@ -1,5 +1,12 @@
 string = input()
 
 # 첫번째 방법 : slicing
-string = string[:1] + string[2:-2] + string[-1:]
-print(string) #
+#string = string[:1] + string[2:-2] + string[-1:]
+
+
+# 두번째 방법 : pop
+string = list(string)
+string.pop(1)
+string.pop(-2)
+string = ''.join(string)
+print(string)
