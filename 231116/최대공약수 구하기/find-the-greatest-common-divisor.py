6e59b86cd71a19abad1b7fa6049e8e3751d5d7e6
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-16 08:48:10 +0900

    Title: 최대공약수 구하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 231116/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,13 +1,12 @@
-def print_gcd(num1, num2) :
-    comman_divisor = 1
-    for i in range(1,101) :
-        if i > num1 or i > num2 :
-            break
+n,m = tuple(map(int, input().split()))
 
-        if num1%i == 0 and num2%i == 0 :
-            comman_divisor = i
+def find_gcd(n,m) :
+    gcd = 0
+    for i in range(1, min(n,m) +1) :
+        if n % i == 0 and m % i == 0 :
+            gcd = i
+        
+    print(gcd)
 
-    print(comman_divisor)
 
-n1, n2 = tuple(map(int, input().split()))
-print_gcd(n1, n2)
+find_gcd(n,m)
