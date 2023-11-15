46bf5e20657d73d6dd511053583fce5f9db57994
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-15 12:33:41 +0900

    Title: 그 다음 알파벳, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231115/그 다음 알파벳/next-alphabet.py
+++ 231115/그 다음 알파벳/next-alphabet.py
@@ -1,10 +1,6 @@
 char = input()
 
-next_num = 0
-
-if ord(char)%ord('z') == 0 :
-    next_num = ord('a')
+if char == 'z' :
+    print("a")
 else :
-    next_num = ord(char)%ord('z') + 1
-
-print(chr(next_num))
+    print(chr(ord(char) + 1))
