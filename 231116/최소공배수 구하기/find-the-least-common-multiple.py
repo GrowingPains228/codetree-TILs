f68bd02b3496286498f5fd2184e9835954123e90
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-16 09:48:22 +0900

    Title: 최소공배수 구하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231116/최소공배수 구하기/find-the-least-common-multiple.py
+++ 231116/최소공배수 구하기/find-the-least-common-multiple.py
@@ -5,19 +5,16 @@ def find_lcm(n,m) :
     temp_n = n
     temp_m = m
     #공약수로 나눠서 구하는 방식
-    for i in range(1, min(n,m) +1) :
-        if n%i == 0 and m%i == 0:
-            ldm *= i
+    i = 2
+    while min(temp_n, temp_m) > i :
+        if temp_m % i == 0 and temp_n % i == 0 :
             temp_n //= i
             temp_m //= i
+            ldm *= i
+            i = 2
+            
+        i += 1
 
-    #공약수의 배수중 최소공배수가 존재.
-    cnt = 1
-    while True :
-        if (ldm * cnt) % n == 0 and (ldm * cnt) % m == 0 :
-            print(ldm)
-            break
-        else :
-            cnt += 1
+    print(temp_m * temp_n * ldm)
 
 find_lcm(n,m)
