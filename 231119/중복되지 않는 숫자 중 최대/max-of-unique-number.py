c1609c7390b53c618cf3405eec26f7b92ea3ab88
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-19 21:07:34 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 231119/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 231119/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,21 +1,22 @@
+# 시간 복잡도가 O(N*M)인 방법
+Max_Range = 1000
+
+# 빈도를 나타낼 배열
+arr = [0 for _ in range(Max_Range + 1)]
+
 n = int(input())
-arr = list(map(int, input().split()))
+input_arr = list(map(int, input().split()))
 
-max_number = -1
+# 입력된 배열에서 하나씩 꺼내서 빈도 배열에 등장 빈도를 증감시킨다.
+for elem in input_arr :
+    arr[elem] += 1
 
-for i in range(n) :
-    if max_number < arr[i] :
-        cnt = 0
+# 배열에서 등장 빈도가 1 인 것들만 탐색해서 그중에 제일 큰 수만 등장 시킨다.
+# 뒤에서 부터 탐색하면 더 빠르게 찾을 수 있을 것이다.
+max_num = -1
+for i in range(Max_Range , -1 , -1) :
+    if arr[i] == 1 :
+        max_num = i
+        break
 
-        for j in range(n) :
-            if cnt > 1 :
-                break
-
-            if arr[i] == arr[j] :
-                cnt += 1
-            
-
-        if cnt == 1 :
-            max_number = arr[i]
-
-print(max_number)
+print(max_num)
