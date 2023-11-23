ed87581b9464e71af1bb49ae35699b8d3b44f2fb
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-23 09:54:58 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 231123/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 231123/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -1,24 +1,21 @@
+class Where_Dot :
+    def __init__ (self, idx, value) :
+        self.idx, self.value = idx, value
+
 n = int(input())
 arr = list(map(int, input().split()))
+user_arr = [
+    Where_Dot(i, value) for i, value in enumerate(arr)
+]
 
-order_arr = []
-# 입력된 수열 값과 원래 있던 idx 값(과거의 idx)을 tuple로 만든 타입을 넣어준다.
-for defore_idx in range(n) :
-    order_arr.append((defore_idx,arr[defore_idx]))
+user_arr.sort(key=lambda x : (x.value, x.idx))
 
-# 입력된 값에 의해 정렬을 진행한다.
-order_arr.sort(key=lambda x : x[1])
+order_arr = [
+    0 for _ in range(n)
+]
 
-# 이동한 idx 의 값을 다시 새로운 tuple로 넣어준다.
-arr = []
-for idx_moved in range(n) :
-    defore_idx, value = order_arr[idx_moved] # 언팩킹
-    arr.append((idx_moved, defore_idx, value))
+for i, elem in enumerate(user_arr) :
+    order_arr[elem.idx] = i+1
 
-# defore_idx를 기준으로 오름차순 정렬 진행
-arr.sort(key=lambda x : x[1])
-
-#출력
-for i in range(n) :
-    idx_moved, _, _ = arr[i]
-    print(idx_moved + 1, end = " ")
+for i in order_arr :
+    print(i, end = " ")
