7d76f7ef520bd194a9fcede9549c98bfb02f2d2c
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-24 13:53:38 +0900

    Title: 왔다 갔던 구역 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231124/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 231124/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -13,20 +13,22 @@ current_idx = 0
 for _ in range(n) :
     x,Ro = tuple(input().split())
     x = int(x)
-    in_de = 1   # 증감 변수
+    in_de = 1   # 증감 변수 정의
 
     # L 이라면 왼쪽으로 가야하므로, 이에대한 Offest 업데이트
     if Ro == "L" :
         x *= -1
         in_de = -1
 
+    # 종료될 인덱스 저장
+    end_idx = current_idx + x
+
     # 지나간 경로 연산
-    for i in range(current_idx, current_idx + x, in_de) :
+    for i in range(current_idx, end_idx , in_de) :
         road[i] += 1
 
-
     # 현재 인덱스 저장
-    current_idx += x
+    current_idx = end_idx + 1 if end_idx >= current_idx else 0
 
 count = 0
 for elem in road :
