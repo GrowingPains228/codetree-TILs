201fbf2f2eb4468c9a537eb21415d7c65ad7d7ce
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-11-24 16:03:52 +0900

    Title: 왔다 갔던 구역 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231124/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 231124/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -3,25 +3,30 @@ n = int(input())
 road = [0] * 100
 current_idx = 0
 collect_arr = []
+
 #n번에 걸쳐서 입력 받기
 for _ in range(n) :
     x,Ro = tuple(input().split())
     x = int(x)
-    in_de = 1   # 증감 변수 정의
+    in_de = 1   # for문내 증감 변수 정의
+    start_tf = 0
+    end_tf = 0
 
     # L 이라면 왼쪽으로 가야하므로, 이에대한 Offest 업데이트
+    # 오른 쪽으로 갈때는 원래 알고리즘 대로 하면 되지만,
+    # 왼쪽으로 이동할때는 약간의 Offset을 주어 각 끝점에 유의 하여야한다.
     if Ro == "L" :
-        x *= -1
+        start_tf -= 1
+        x *= -1 # 방향
         in_de = -1
+        end_tf = -1
 
     # 종료될 인덱스 저장
     end_idx = current_idx + x
 
     # 지나간 경로 연산
-    for i in range(current_idx, end_idx , in_de) :
+    for i in range(current_idx + start_tf, end_idx + end_tf, in_de) :
         road[i] += 1
-        if road[i] >= 2 and i not in collect_arr  :
-            collect_arr.append(i)
 
     # 현재 인덱스 저장
     current_idx = end_idx
@@ -32,4 +37,3 @@ for elem in road :
         count += 1
 
 print(count)
-#print(len(collect_arr))
