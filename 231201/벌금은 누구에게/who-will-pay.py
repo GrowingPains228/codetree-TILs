4a9130f28da33c249825c3e3cafc0d9d235498f4
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-01 14:18:10 +0900

    Title: 벌금은 누구에게, Time: 175ms, Memory: 28MB, Status: Passed - CodeTree

--- 231201/벌금은 누구에게/who-will-pay.py
+++ 231201/벌금은 누구에게/who-will-pay.py
@@ -1,13 +1,13 @@
 n, m, k = tuple(map(int, input().split()))
 students = [0]*(n + 1)
 
+student = -1
 for _ in range(m) :
     idx = int(input())
-    students[idx] += 1
 
-student = -1
-for i in range(1, n + 1) :
-    if students[i] >= k :
-        student = i
-        break
+    students[idx] += 1
+    
+    if students[idx] >= k and student == -1:
+        student = idx
+
 print(student)
