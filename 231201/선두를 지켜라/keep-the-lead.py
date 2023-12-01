e33959f7b1a09a6092651cc4b292cb43241d5cfa
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-01 18:22:02 +0900

    Title: 선두를 지켜라, Time: 169ms, Memory: 88MB, Status: Passed - CodeTree

--- 231201/선두를 지켜라/keep-the-lead.py
+++ 231201/선두를 지켜라/keep-the-lead.py
@@ -4,32 +4,38 @@ n,m = tuple(map(int, input().split()))
 road_a = [0] * (MAX_RANGE + 1)
 road_b = [0] * (MAX_RANGE + 1)
 
+# 시간과 속도에 따른 이동 거리를 배열에 기입한다.
 idx_a = 1
 for _ in range(n) :
     verse, time = tuple(map(int, input().split()))
-    distance = verse/time
     for _ in range(time) :
-        road_a[idx_a] += distance
+        road_a[idx_a] = road_a[idx_a - 1] + verse
         idx_a += 1
-
 
 idx_b = 1
 for _ in range(m) :
     verse, time = tuple(map(int, input().split()))
-    distance = verse/time
     for _ in range(time) :
-        road_b[idx_b] += distance
+        road_b[idx_b] = road_b[idx_b- 1] + verse
         idx_b += 1
 
+
+#이동거리 배열을 비교하면서 선두가 몇번 바뀌었는지 체크한다.
 cnt = 0
-for i in range(1, idx_a) :
-    if road_a[i] > road_b[i] :
-        if road_a[i - 1] < road_b[i - 1] :
-            cnt += 1
+# 1은 a가 선두, 2는 b가 선두
+leader = (1 if road_a[1] > road_b[1] else 2)
+for i in range(2, idx_a) :
+    if leader == 1 :
+        if road_a[i] >= road_b[i] :
             continue
-    
-    if road_a[i] < road_b[i] :
-        if road_a[i - 1] > road_b[i - 1] :
-            cnt += 1
+
+        leader = 2
+        cnt += 1
+    else :
+        if road_a[i] <= road_b[i] :
+            continue
+
+        leader = 1
+        cnt += 1
 
 print(cnt)
