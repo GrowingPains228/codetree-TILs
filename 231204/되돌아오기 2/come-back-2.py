5f91671fc5f40787408c2f5f6ad6997f80d106ac
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-04 23:02:52 +0900

    Title: 되돌아오기 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231204/되돌아오기 2/come-back-2.py
@@ -0,0 +1,22 @@
+commands = input()
+
+dxs, dys = [0, 1,  0, -1],[1, 0, -1, 0 ]
+
+dir_num = 0
+x,y = 0,0
+ans = -1
+elapmed_cnt = 0
+for command in commands :
+    if command == 'F':
+        x,y = x + dxs[dir_num], y + dys[dir_num]
+    elif command == 'R' :
+        dir_num = (dir_num + 1) % 4
+    else :
+        dir_num = (3 - dir_num) % 4
+    
+    elapmed_cnt += 1
+    if x == 0 and y == 0 :
+        ans = elapmed_cnt
+        break
+        
+print(ans)
