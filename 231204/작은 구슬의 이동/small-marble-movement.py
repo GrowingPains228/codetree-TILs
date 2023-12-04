0043bc014409abb4d671e8acafeb6fae9b48d507
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-04 21:40:46 +0900

    Title: 작은 구슬의 이동, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/작은 구슬의 이동/small-marble-movement.py
+++ 231204/작은 구슬의 이동/small-marble-movement.py
@@ -3,7 +3,7 @@ n, t = tuple(map(int, input().split()))
 def in_range(x, y) : 
     return 0 <= x and x < n and 0 <= y and y < n
 
-dxs, dys = [1, 0, 0, -1], [0, 1, -1, 0]
+dxs, dys = [-1, 0, 0, 1], [0, 1, -1, 0]
 
 direct = {
     'U' : 0,
@@ -17,7 +17,7 @@ r, c, d = tuple(input().split())
 d_type = direct[d]
 r,c = int(r), int(c)
 x,y = r-1, c-1
-cnt, ans = 0, 0
+
 for _ in range(t) :
     if in_range(x + dxs[d_type], y + dys[d_type]) :
         x,y = x + dxs[d_type], y + dys[d_type]
