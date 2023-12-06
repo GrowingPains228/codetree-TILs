13d7d8e469f2335cb914d24f054e266035aa0d49
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-06 15:14:39 +0900

    Title: 이상한 진수 2, Time: 217ms, Memory: 24MB, Status: Passed - CodeTree

--- 231206/이상한 진수 2/awkward-digits-2.py
+++ 231206/이상한 진수 2/awkward-digits-2.py
@@ -8,13 +8,10 @@ def change_decimal(bin_str) :
 
 max_num = 0
 for i in range(len(binary_string)) :
-    if binary_string[i] != '0' :
-        continue
-    
-    binary_string[i] = '1'
+    binary_string[i] = '0' if binary_string[i] == '1' else '1'
     change_num = change_decimal(binary_string)
     max_num = max(max_num, change_num)
 
-    binary_string[i] = '0'
+    binary_string[i] = '0' if binary_string[i] == '1' else '1'
 
 print(max_num)
