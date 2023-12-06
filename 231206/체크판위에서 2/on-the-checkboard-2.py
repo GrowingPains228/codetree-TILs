1a46c0f841bc2fb6d0a118469f32f22da2c5800c
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-06 14:01:13 +0900

    Title: 체크판위에서 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231206/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,21 @@
+r,c = tuple(map(int, input().split()))
+arr = [
+    input().split() 
+    for _ in range(r)
+]
+
+# 각 위치에서 이동할 수 있는 깊이를 나타냄.
+count_arr = [
+    [0]*c
+    for _ in range(r) 
+]
+
+cnt = 0
+for i in range(1, r-2) :
+    for j in range(1, c-2) :
+        if arr[0][0] != arr[i][j] :
+            for k in range(i + 1, r-1) :
+                for z in range(j + 1, c-1) :
+                    if arr[i][j] != arr[k][z] :
+                        cnt += 1
+print(cnt)
