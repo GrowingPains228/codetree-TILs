01d259f2d95ae9a3d7e97da49cf1a5d76572115f
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-06 10:59:21 +0900

    Title: 일렬로 서있는 소 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231206/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n) :
+    for j in range(i+1, n) :
+        for k in range(j + 1, n) :
+            if arr[i] <= arr[j] and arr[j] <= arr[k] :
+                cnt += 1
+
+print(cnt)
