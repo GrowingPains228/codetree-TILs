1225ca3fb1b47e624c65a8e313eeae0bae98d04b
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-08 11:29:49 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231208/G or H 3/G-or-H-3.py
@@ -0,0 +1,19 @@
+MAX_RANGE = 100
+n,k = tuple(map(int, input().split()))
+
+arr = [0] * (MAX_RANGE + 1)
+
+for _ in range( n) :
+    idx, value = tuple(input().split())
+    arr[int(idx)] = 1 if value == 'G' else 2
+
+ans = 0
+
+for i in range(1, n - k + 2) :
+    sum_value = 0
+    for j in range(i, i + k +1) :
+        sum_value += arr[j]
+
+    ans = max(ans, sum_value)
+
+print(ans)
