79fcc1a35fe070794b1506670f4bc842f1a3c9b3
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-09 23:56:52 +0900

    Title: 바구니 안의 사탕 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 231209/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 231209/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -8,8 +8,13 @@ for _ in range(n) :
     arr[idx] += nums
 
 ans = 0
-for i in range(1, (MAX_RANGE + 1) - 2*k ) :
-    nums_sum = sum(arr[i : i + 2*k + 1])
-    ans = max(nums_sum, ans)
+if k >= (MAX_RANGE//2) :
+    for i in range(1, MAX_RANGE + 1 ) :
+        nums_sum = sum(arr[i : i + 2*k + 1])
+        ans = max(nums_sum, ans)
+else :
+    for i in range(1, (MAX_RANGE + 1) - 2*k ) :
+        nums_sum = sum(arr[i : i + 2*k + 1])
+        ans = max(nums_sum, ans)
 
 print(ans)
