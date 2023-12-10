c9052a9198858881263812142e0322378344e416
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-11 07:57:45 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 177ms, Memory: 26MB, Status: Passed - CodeTree

--- 231211/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 231211/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -1,23 +1,11 @@
 n = int(input())
 pass1, pass2, pass3 = tuple(map(int, input().split()))
 
-def in_range(n1, n2, n3) :
-    if abs(pass1-n1) <= 2 :
-        return True
-    
-    if abs(pass2-n2) <= 2 :
-        return True
-
-    if abs(pass3-n3) <= 2 :
-        return True
-    
-    return False
-
 cnt = 0
 for i in range(1,n+1) :
     for j in range(1,n+1) :
         for k in range(1, n+1) :
-            if in_range(i, j, k) :
+            if abs(pass1-i) <= 2 or abs(pass2-j) <= 2 or abs(pass3-k) <= 2 :
                 cnt += 1
 
 print(cnt)
