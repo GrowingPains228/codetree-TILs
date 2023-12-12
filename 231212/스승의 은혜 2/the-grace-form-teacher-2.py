619f511061f8bd65c95b13f859457c6d39eb9914
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-12 20:26:28 +0900

    Title: 스승의 은혜 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231212/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,20 @@
+n,b = tuple(map(int, input().split()))
+
+presents = [
+    int(input()) for _ in range(n)
+]
+
+presents.sort()
+
+max_students = 0
+for i in range(n) :
+    cash = b
+    students = 0
+    for j in range(n) :
+        if cash >= presents[j] :
+            cash -= (presents[j]//2) if j == i else presents[j]
+            students += 1
+        
+    max_students = max(max_students, students)
+
+print(max_students)
