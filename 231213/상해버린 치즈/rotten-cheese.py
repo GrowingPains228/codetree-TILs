9b8b89a0529741f349f0749c35da0d7a3ab2c478
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-13 13:45:19 +0900

    Title: 상해버린 치즈, Time: 137ms, Memory: 28MB, Status: Passed - CodeTree

--- 231213/상해버린 치즈/rotten-cheese.py
+++ 231213/상해버린 치즈/rotten-cheese.py
@@ -22,13 +22,13 @@ for i in range(s) :
         if person == who :
             if when < time :
                 cheese[what-1] += 1 # 상했을 가능성이 있는 치즈에 가중치를 준다
-            else :
-                cheese[what-1] -= 1 # 상했을 가능성이 없는 치즈라 판단, 가중치 감소
+            # else :
+            #     cheese[what-1] -= 1 # 상했을 가능성이 없는 치즈라 판단, 가중치 감소
 
 # 상한 치즈에 대한 가중치는 아픈 기록수와 같은 치즈에 대해서 가장 의심하면 된다.
 for i in range(m) :
     # 상한 치즈를 먹은 사람들 업데이트
-    if cheese[i] == s :
+    if cheese[i] >= s :
         for (who, what, when) in eat_notes :
             if (i + 1) == what :
                 people[who-1] = True
