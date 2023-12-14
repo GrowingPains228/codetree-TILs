4de800fa5ad8f8300fbea0793f435c6be5f10c10
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-14 15:41:09 +0900

    Title: 정보에 따른 숫자 2, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231214/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 231214/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -2,25 +2,29 @@ MAX_LINE = 1000
 T,a,b = tuple(map(int, input().split()))
 
 arr = [''] * (MAX_LINE + 1)
-unique_arr = [True] * (MAX_LINE + 1)
+unique_arr = [False] * (MAX_LINE + 1)
 for _ in range(T) :
     alpha, position = tuple(input().split())
     arr[int(position)] = alpha
 
 cnt = 0
-for i in range(0,MAX_LINE+1) :
+for i in range(1,MAX_LINE+1) :
     if arr[i] == '' :
         continue
-    
-    alpha1 = arr[i]
+    # S,N을 만날것이고,
     for j in range(i+1, MAX_LINE+1) :
-        if arr[j] == '' or arr[j] == alpha1 :
+        if arr[j] == '' :
             continue
         
-        alpha2 = arr[j]
+        # 가까운 위치에 똑같은 놈이 있으면, 이 for문을 빠져나가서
+        # 방금 그놈을 잡도록 한다.
+        if arr[j] == arr[i] :
+            for z in range(i,j + 1) :
+                unique_arr[z] = True
+            break
 
         start, end = 0,0
-        if alpha1 == 'S' :
+        if arr[i] == 'S' :
             start, end = (i+j)//2, i
         else :
             start, end = j, (i+j)//2 if (i+j) % 2 == 0 else (i+j)//2 + 1
