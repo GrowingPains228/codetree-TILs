fcffa9881900058be52f8ab032b1d41f381ec67d
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-14 14:35:41 +0900

    Title: 정보에 따른 숫자 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231214/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 231214/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -2,17 +2,18 @@ MAX_LINE = 1000
 T,a,b = tuple(map(int, input().split()))
 
 arr = [''] * (MAX_LINE + 1)
+unique_arr = [True] * (MAX_LINE + 1)
 for _ in range(T) :
     alpha, position = tuple(input().split())
     arr[int(position)] = alpha
 
 cnt = 0
-for i in range(a,b+1) :
+for i in range(0,MAX_LINE+1) :
     if arr[i] == '' :
         continue
     
     alpha1 = arr[i]
-    for j in range(i+1, b+1) :
+    for j in range(i+1, MAX_LINE+1) :
         if arr[j] == '' or arr[j] == alpha1 :
             continue
         
@@ -24,9 +25,9 @@ for i in range(a,b+1) :
         else :
             start, end = j, (i+j)//2 if (i+j) % 2 == 0 else (i+j)//2 + 1
 
-        for _ in range(start, end-1, -1) :
-            cnt += 1
+        for k in range(start, end-1, -1) :
+            unique_arr[k] = True
 
         break
 
-print(cnt)
+print(sum(unique_arr[a:b+1]))
