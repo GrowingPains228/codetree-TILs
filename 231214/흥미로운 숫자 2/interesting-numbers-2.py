9b4869231d5c035a508e4dd41329d2ceee3bceb2
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-14 11:32:13 +0900

    Title: 흥미로운 숫자 2, Time: 194ms, Memory: 25MB, Status: Passed - CodeTree

--- 231214/흥미로운 숫자 2/interesting-numbers-2.py
+++ 231214/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,13 +1,22 @@
 x,y = tuple(map(int, input().split()))
 
 def is_Interesting(num) :
-    #set 을 이용해서 간단하게 구현하기
-    s = list(str(num))
-    s = set(s)
-    if len(s) == 2:
-        return True
-    else :
-        return False
+    count = [0] * 10
+
+    while num > 0 :
+        count[num%10] += 1
+        num //= 10
+
+    cnt_1 = 0
+    cnt_2 = 0
+    for i in range(10) :
+        if count[i] >= 2:
+            cnt_2 += 1
+            
+        if count[i] >= 1:
+            cnt_1 += 1
+    
+    return (cnt_1 == 2 and cnt_2 == 1)
 
 cnt = 0
 for i in range(x, y+1) : 
