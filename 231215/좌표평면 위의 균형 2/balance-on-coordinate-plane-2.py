651bed7bf7c79715b79b070a32d83af70f181395
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-15 23:01:39 +0900

    Title: 좌표평면 위의 균형 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231215/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,47 @@
+import sys
+INT_MAX = sys.maxsize
+n = int(input())
+
+dots = [ ]
+x_elem = []
+y_elem = []
+for _ in range(n) :
+    x,y = tuple(map(int ,input().split()))
+
+    dots.append((x,y))
+
+    if x not in x_elem :
+        x_elem.append(x)
+    if y not in y_elem :
+        y_elem.append(y)
+
+x_elem.sort()
+y_elem.sort()
+# 최소값은, x의 가운데 쯤과, y 의 가운데 쯤일 것이다.
+# 따라서, 정렬을 한다음 가운데에 있는 값들 사이를 조사하면 될것이다.
+ans = 0
+diff_dots = INT_MAX
+for i in range(len(x_elem)-1) :
+    x_axis = x_elem[i] + 1
+    for j in range(len(y_elem)-1) :
+        y_axis = y_elem[j] +1
+
+        count = [0] * 4
+
+        for x,y in dots :
+            if x > x_axis :
+                if y > y_axis :
+                    count[0] += 1
+                else :
+                    count[1] += 1
+            else :
+                if y > y_axis :
+                    count[2] += 1
+                else :
+                    count[3] += 1
+
+        if diff_dots > (max(count) - min(count)) :
+            diff_dots = (max(count) - min(count))
+            ans = max(count)
+
+print(ans)
