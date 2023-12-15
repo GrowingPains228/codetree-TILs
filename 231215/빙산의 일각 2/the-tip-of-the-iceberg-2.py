9a751ad2a71e8edb1cc576876032027ad757d4fe
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-15 13:44:26 +0900

    Title: 빙산의 일각 2, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231215/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -0,0 +1,29 @@
+n = int(input())
+arr = [
+    int(input()) for _ in range(n)
+]
+
+# True 이면 현재 빙산의 개수를 센것이다.
+
+ans = 0
+for i in range(n) :
+    ground = arr[::]
+
+    for k in range(n) :
+        ground[k] -= ground[i]
+
+    is_ice = False
+    cnt = 0
+    for j in range(n) :
+        if ground[j] <= 0 :
+            is_ice = False
+
+        elif ground[j] > 0 :
+            if is_ice :
+                continue
+
+            is_ice = True
+            cnt += 1
+    ans = max(ans, cnt)
+    
+print(ans)
