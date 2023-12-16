c3862532574e337c646231f3ee785a649433667b
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-16 11:42:45 +0900

    Title: 팀으로 하는 틱택토 2, Time: 143ms, Memory: 73MB, Status: Passed - CodeTree

--- 231216/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 231216/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -6,6 +6,7 @@ TickTackTo = [
     for _ in range(3)
 ]
 
+team = []
 ans = 0
 # 가로
 for i in range(3) :
@@ -14,8 +15,9 @@ for i in range(3) :
         if elem not in segment :
             segment.append(elem)
 
-    if len(segment) == 2 :
+    if len(segment) == 2 and segment not in team:
         ans += 1
+        team.append(segment)
 
 # 세로
 for i in range(3) :
@@ -24,8 +26,9 @@ for i in range(3) :
         if TickTackTo[j][i] not in segment :
             segment.append(TickTackTo[j][i])
 
-    if len(segment) == 2 :
+    if len(segment) == 2  and segment not in team:
         ans += 1
+        team.append(segment)
 
 # 대각선
 segment = []
@@ -33,15 +36,17 @@ for i in range(3) :
     if TickTackTo[i][i] not in segment :
         segment.append(TickTackTo[i][i])
 
-if len(segment) == 2 :
+if len(segment) == 2  and segment not in team:
     ans += 1
+    team.append(segment)
 
 segment = []
 for i in range(3) :
     if TickTackTo[i][2-i] not in segment :
         segment.append(TickTackTo[i][2-i])  
 
-if len(segment) == 2:
+if len(segment) == 2  and segment not in team:
     ans += 1
+    team.append(segment)
 
 print(ans)
