90404bdcaad96d064f17b9b3f655f0e016aa0c4b
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-19 21:03:51 +0900

    Title: 독서실의 거리두기 5, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 231219/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 231219/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,35 +1,24 @@
 n = int(input())
-seats = list(map(int, list(input())))
+seats = list(input())
 
-idx = 0
-re_distance = 0
-for i in range(n) :
-    if seats[i] == 0 :
-        # 임의로 좌석에 앉히고 거리를 측정한다.
-        seats[i] = 1
-        distance = n + 1
-        for j in range(n) :
-            if seats[j] == 1 :
-                for k in range(j+1, n) :
-                    if j == k :
-                        continue
-                    # 가장 가까운 거리에 있는 좌석의 거리값을 측정한다.
-                    if seats[k] == 1 :
-                        distance = min(distance, abs(k - j))
-        # 그 거리값이 가장 큰 것을 가져온다.
-        if re_distance < distance :
-            idx = i
-            re_distance = distance
+def min_dist() :
+    dist = n
 
-        seats[i] = 0
-seats[idx] = 1
-ans = n + 1
+    for i in range(n) :
+        for j in range(i + 1, n) :
+            if seats[i] == '1' and seats[j] == '1' :
+                dist = min(dist, j - i)
+    
+    return dist
+
+ans = 0
+
 for i in range(n) :
-    for j in range(n) :
-        if i == j :
-            continue
-        
-        if seats[i] == 1 and seats[j] == 1 :
-            ans = min(abs(i-j), ans)
+    if seats[i] == '0' :
+        seats[i] = '1'
+
+        ans = max(ans, min_dist())
+
+        seats[i] = '0'
 
 print(ans)
