bbbd300e48fdb4157a142ec110151fd33c37a0be
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-19 21:00:11 +0900

    Title: 독서실의 거리두기 5, Time: 142ms, Memory: 73MB, Status: Passed - CodeTree

--- 231219/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 231219/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -2,25 +2,26 @@ n = int(input())
 seats = list(map(int, list(input())))
 
 idx = 0
+re_distance = 0
 for i in range(n) :
     if seats[i] == 0 :
+        # 임의로 좌석에 앉히고 거리를 측정한다.
         seats[i] = 1
-        re_distance = 0
+        distance = n + 1
         for j in range(n) :
-            distance = n + 1
-            for k in range(n) :
-                if j == k :
-                    continue
-
-                if seats[j] == 1 and seats[k] == 1 :
-                    distance = min(distance, abs(j-k))
-            
-            if re_distance < distance :
-                idx = i
-                re_distance = distance
+            if seats[j] == 1 :
+                for k in range(j+1, n) :
+                    if j == k :
+                        continue
+                    # 가장 가까운 거리에 있는 좌석의 거리값을 측정한다.
+                    if seats[k] == 1 :
+                        distance = min(distance, abs(k - j))
+        # 그 거리값이 가장 큰 것을 가져온다.
+        if re_distance < distance :
+            idx = i
+            re_distance = distance
 
         seats[i] = 0
-
 seats[idx] = 1
 ans = n + 1
 for i in range(n) :
