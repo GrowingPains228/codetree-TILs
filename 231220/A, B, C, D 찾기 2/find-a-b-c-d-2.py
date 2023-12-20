183a08ff641c04aaff340af2b80fc30e2d530aa2
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-20 14:03:20 +0900

    Title: A, B, C, D 찾기 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 231220/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 231220/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,15 +1,24 @@
 import sys
 
-MAX_NUM = 40
 arr = list(map(int, input().split()))
 arr.sort()
+MAX_NUM = max(arr)
+
 for a in range(1, MAX_NUM + 1) :
-    if a in arr:
-        for b in range(1, MAX_NUM + 1) :
-            if b in arr and a+b in arr :
-                abcd = max(arr)
-                for c in range(1, abcd - (a+b) + 1) :
-                    if c in arr and a+c in arr and b+c in arr and a+b+c in arr :
-                        d = abcd - (a+b+c)
-                        print(a, b, c, d)
-                        sys.exit()
+    if arr[0] == a :
+        for b in range(a, MAX_NUM + 1 - a) :
+            if arr[1] == b :
+                for c in range(b, MAX_NUM + 1 - a - b) :
+                    for d in range(c, MAX_NUM + 1 - a - b - c) :
+                        new_arr = [a,b,c,d,a+b,b+c,c+d,d+a,a+c,b+d,a+b+c,a+b+d,a+c+d,b+c+d,a+b+c+d]
+                        new_arr.sort()
+
+                        is_same = True
+                        for num1, num2 in zip(new_arr, arr) :
+                            if num1 != num2 :
+                                is_same = False
+                                break
+                        
+                        if is_same :
+                            print(a,b,c,d)
+                            sys.exit()
