97630ee982e8a1bea725b87a2b897fa979cd1a81
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-26 00:57:56 +0900

    Title: 달리자, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 231226/달리자/run-and-run.py
+++ 231226/달리자/run-and-run.py
@@ -2,21 +2,13 @@ n = int(input())
 before = [0] + list(map(int, input().split()))
 after = [0] + list(map(int, input().split()))
 
-tmp_arr = before[::]
 ans = 0
-for i in range(n, 0, -1) :
-    if after[i] == tmp_arr[i] :
-        continue
 
-    moved_person = after[i] - tmp_arr[i]
-    for j in range(i-1, 0, -1) :
-        if moved_person == 0 :
-            break
-
-        count_person = tmp_arr[j] if moved_person >= tmp_arr[j] else moved_person
-        tmp_arr[i] += count_person
-        ans += count_person * (i - j)
-        tmp_arr[j] -= count_person
-        moved_person -= count_person
+for i in range(1, n+1) :
+    if before[i] > after[i] :
+        cnt = before[i] - after[i]
+        before[i+1] += cnt
+        before[i] -= cnt
+        ans += cnt
 
 print(ans)
