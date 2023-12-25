22d2212e3f35d8f79b6e45f2cc17097021a8d898
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-25 22:29:30 +0900

    Title: 전부 겹치는 선분 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231225/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,16 @@
+MAX_RANGE = 100
+n = int(input())
+
+max_x1 = 0
+min_x2 = MAX_RANGE
+cnt = 0
+for _ in range(n) :
+    x1, x2 = tuple(map(int, input().split()))
+    if x1 > min_x2 or x2 < max_x1 :
+        continue
+    
+    max_x1 = max(max_x1, x1)
+    min_x2 = min(min_x2, x2)
+    cnt += 1
+
+print("Yes" if cnt == n-1 else "No")
