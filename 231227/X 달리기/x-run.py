588ee17049626876f26f3ae6823309cea45a3e8a
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-27 19:33:55 +0900

    Title: X 달리기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231227/X 달리기/x-run.py
+++ 231227/X 달리기/x-run.py
@@ -1,27 +1,20 @@
-# 거리 입력 받기
 x = int(input())
+t = 0
+left_dist = x
+v = 1
 
-# 현재 속도
-cur_v = 1
-# 현재 거리
-dis = 0
-#현재 소요 시간
-time = 0
+while True:
+    left_dist -= v
+    t += 1
 
-# 최고 속도 까지 올리기
-while dis < x//2 :
-    next_d = cur_v * 1
-    if dis + next_d >= x//2 :
+    if left_dist == 0:
         break
-    time += 1
-    dis += next_d
-    cur_v += 1
-
-if x//2 == dis :
-    time *= 2
-elif x - dis*2 > cur_v :
-    time = 2*time + 2
-else :
-    time = 2*time + 1
-
-print(time)
+    
+    if left_dist >= (v + 1) * (v + 2) / 2:
+        v += 1
+    elif left_dist >= v * (v + 1) / 2:
+        pass
+    else :
+        v -= 1
+    
+print(t)
