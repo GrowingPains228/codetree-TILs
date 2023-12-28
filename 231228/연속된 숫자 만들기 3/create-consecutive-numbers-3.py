b1ec794090a1b6b8f65a4e1d7b3bec8d3c31c773
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-28 10:38:54 +0900

    Title: 연속된 숫자 만들기 3, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 231228/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -4,18 +4,7 @@ ans = 0
 if line[0] + 1 == line[1] and line[1]+1 == line[2] :
     ans = 0
 else :
-    while True :
-        if line[0] + 1 == line[1] and line[1]+1 == line[2] :
-            break
-        
-        # 사이가 가장 먼 저리 구하기
-        if line[1]- line[0] < line[2] - line[1] :
-            line[0] = line[1] + 1
-            line[0], line[1] = line[1], line[0]
-        else :
-            line[2] = line[1] - 1
-            line[1], line[2] = line[2], line[1]
-        
-        ans += 1
+    # 사이가 가장 먼 저리 구하기
+    ans = max(line[1]- line[0] -1, line[2] - line[1] -1)
 
 print(ans)
