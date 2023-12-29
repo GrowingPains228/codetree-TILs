4596905f8ee34c341351bb4e773515f09469d17b
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-29 21:38:49 +0900

    Title: 최소 와이파이 수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231229/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,14 @@
+n,m = tuple(map(int, input().split()))
+seat = list(map(int, input().split()))
+
+cnt = 0
+idx = 0
+while idx < n :
+    if seat[idx] == 0 :
+        idx += 1
+    
+    # 손님이 앉아 있는데, 와이파이 존이 아니라면, 
+    cnt += 1 #와이파이 설치하고 
+    idx += m * 2 + 1 # 영향권 밖으로 보내버리기
+
+print(cnt)
