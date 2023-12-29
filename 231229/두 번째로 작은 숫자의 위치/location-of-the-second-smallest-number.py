14a0fc2298d5da3776800880f1a673eed8fd1727
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-29 15:57:17 +0900

    Title: 두 번째로 작은 숫자의 위치, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231229/두 번째로 작은 숫자의 위치/location-of-the-second-smallest-number.py
@@ -0,0 +1,26 @@
+import sys
+n = int(input())
+numbers = list(map(int, input().split()))
+temp_arr = numbers[::]
+temp_arr.sort()
+# 두번째로 작은 숫자가 무엇인지 일단 판별
+first = temp_arr[0]
+second = -1
+for i in range(1, n) :
+    if temp_arr[i] == first :
+        continue
+    
+    second = temp_arr[i]
+    # 두번째로 작은 숫자가 여러개라면 -1 출력
+    break
+    
+if second == -1 :
+    print(-1)
+    sys.exit()
+
+for i in range(n) :
+    if numbers[i] == second :
+        second = i
+        break   
+
+print(second+1)
