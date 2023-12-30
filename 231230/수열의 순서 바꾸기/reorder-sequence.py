b750787477ad53ba3754a7b14d88c70b1025f2f9
GrowingPains228 <150199521+GrowingPains228@users.noreply.github.com>
2023-12-30 14:01:50 +0900

    Title: 수열의 순서 바꾸기, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 231230/수열의 순서 바꾸기/reorder-sequence.py
+++ 231230/수열의 순서 바꾸기/reorder-sequence.py
@@ -3,9 +3,11 @@ arr = list(map(int, input().split()))
 
 # 1. 오름차순이 맞지 않는 큰 숫자가 중간에 있으면, 제일 앞에서부터 큰 숫자까지 뒤로 이동해야한다.
 idx = 0
+is_asc = True
 for i in range(n-1) :
     if arr[i] > arr[i+1] :
         idx = i
+        is_asc = False
 
 
-print(idx+1)
+print(idx if is_asc else idx + 1)
