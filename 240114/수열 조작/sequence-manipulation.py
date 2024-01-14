94befb02a49515017b807b423d1d625b4bb82420
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-01-14 18:26:11 +0900

    Title: 수열 조작, Time: 211ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240114/수열 조작/sequence-manipulation.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+n = int(input())
+q = deque()
+for i in range(1, n+1) :
+    q.append(i)
+
+while len(q) > 1 : 
+    q.popleft()
+    q.append(q[0])
+    q.popleft()
+
+print(q.pop())
