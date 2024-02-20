68bac7c2c271eeba8f947c6939bd34bb097e76cb
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-02-21 08:54:15 +0900

    Title: 트로미노, Time: 229ms, Memory: 26MB, Status: Passed - CodeTree

--- 240221/트로미노/tromino.py
+++ 240221/트로미노/tromino.py
@@ -14,21 +14,19 @@ def is_in_range(x,y, block) :
     
     return True
         
-
-# 첫번째 블록
-type1_shape = [[(0,0), (0,1), (1,1)], 
+# 첫번째 블록 + 두번째 블록
+type_shape = [[(0,0), (0,1), (1,1)], 
                 [(0,0),(1,0),(1,1)],
                 [(0,0), (0,1), (-1,1)],
-                [(0,0),(-1,0),(-1,1)]]
-# 두번째 블록
-type2_shape = [[(0,0), (1,0), (2,0)],
+                [(0,0),(-1,0),(-1,1)],
+                [(0,0), (1,0), (2,0)],
                 [(0,0), (0,1),(0,2)]]
 
 max_sum = 0
 for i in range(n) :
     for j in range(m) :
         # 첫번째 블럭
-        for block in type1_shape :
+        for block in type_shape :
             if not is_in_range(j,i, block) : # 범위안에 포함되어있지 않다면, 
                 continue                         # 패스
             
@@ -39,16 +37,4 @@ for i in range(n) :
 
             max_sum = max(max_sum, sum_num)
         
-        # 두번째 블럭
-        for block in type2_shape :
-            if not is_in_range(j,i, block) : # 범위안에 포함되어있지 않다면, 
-                continue                         # 패스
-            
-            sum_num = 0
-            for elem in block :
-                dx, dy = elem
-                sum_num += grid[i+dy][j+dx]
-
-            max_sum = max(max_sum, sum_num)
-
 print(max_sum)
