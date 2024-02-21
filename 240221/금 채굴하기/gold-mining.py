5457c3f3a66fbde3891e37229d0c946d4df7e060
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-02-21 11:31:29 +0900

    Title: 금 채굴하기, Time: 298ms, Memory: 26MB, Status: Passed - CodeTree

--- 240221/금 채굴하기/gold-mining.py
+++ 240221/금 채굴하기/gold-mining.py
@@ -5,7 +5,7 @@ grid = [
 ]
 
 def CostMining(x) :
-    return x * x + (x+1) * (x+1)
+    return x **2 + (x+1)**2
 
 # 먼저 총 황금의 개수를 세보는게 k 의 범위를 설정하는데 도움이 되지 않을까?
 # 시간적 낭비일까?
@@ -25,30 +25,37 @@ while CostMining(max_k) <= Total_Gold_Cost:
 
 # k 거리안에 황금을 채굴할 수 있는 격자의 수를 세어보기
 def draw_diamond_n_Count_Gold(x,y):
-    cnt_gold = 0
-    # 높이 양수 범위
-    for dy in range(max_k) :
-        for dx in range(-max_k + dy + 1, max_k - dy) :
-            rx, ry = x + dx, y + dy
-            if rx < 0 or rx >= n or ry >= n :
-                continue
-            if grid[ry][rx] == 0 :
-                continue
-            else :
-                cnt_gold += 1
+    global m
+    return_Gold = 0
+    for k in range(max_k) :
+        # 높이 양수 범위
+        sum_gold = 0
+        for dy in range(k+1) :
+            for dx in range(-k + dy, k - dy+1) :
+                rx, ry = x + dx, y + dy
+                if rx < 0 or rx >= n or ry >= n :
+                    continue
 
-    #높이 음수 범위
-    for dy in range(-max_k + 1, 0) :
-        for dx in range(-max_k - dy + 1, max_k + dy) :
-            rx, ry = x + dx, y + dy
-            if rx < 0 or rx >= n or ry >= n or ry < 0 :
-                continue
-            if grid[ry][rx] == 0 :
-                continue
-            else :
-                cnt_gold += 1
+                if grid[ry][rx] == 0 :
+                    continue
+                else :
+                    sum_gold += 1
+
+        #높이 음수 범위
+        for dy in range(-k, 0) :
+            for dx in range(-k - dy, k + dy+1) :
+                rx, ry = x + dx, y + dy
+                if rx < 0 or rx >= n or ry >= n or ry < 0 :
+                    continue
+                if grid[ry][rx] == 0 :
+                    continue
+                else :
+                    sum_gold += 1
+
+        if CostMining(k) <= sum_gold * m :
+            return_Gold = max(return_Gold, sum_gold)
             
-    return cnt_gold
+    return return_Gold
 
 ans = 0
 for i in range(n) :
