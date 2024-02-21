454b59468d5eb8588eddb90e273bf4041afb3c22
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-02-22 08:34:12 +0900

    Title: 금 채굴하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240222/금 채굴하기/gold-mining.py
@@ -0,0 +1,33 @@
+n,m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+# 최악의 경우 k
+max_k = 2*(n-1)
+
+def cost_land(k) :
+    return k * k + (k+1) * (k+1)
+
+
+# 범위 안에 황금 찾아주기
+def function(x,y,k) :
+    cnt_gold = 0
+    for i in range(n) :
+        for j in range(n) :
+            if abs(j-x) + abs(i-y) <= k :
+                cnt_gold += grid[i][j]
+    
+    return cnt_gold
+
+cnt_gold = 0
+for i in range(n) :
+    for j in range(n) :
+        for k in range(max_k) :
+            golds = function(j,i,k)
+
+            if golds * m >= cost_land(k) :
+                cnt_gold = max(cnt_gold, golds)
+
+print(cnt_gold)
