2ee2bfb564fd9e1fefe4657ddd72c7e8f200ec2b
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-02-21 09:09:55 +0900

    Title: 트로미노, Time: 237ms, Memory: 26MB, Status: Passed - CodeTree

--- 240221/트로미노/tromino.py
+++ 240221/트로미노/tromino.py
@@ -1,40 +1,56 @@
 n,m = tuple(map(int, input().split()))
-
 grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
-def is_in_range(x,y, block) :
-    for elem in block :
-        dx, dy = elem
-        rx, ry = x + dx, y + dy
-        if rx >= m or rx < 0 or ry >= n or ry < 0 :
-            return False
-    
-    return True
-        
-# 첫번째 블록 + 두번째 블록
-type_shape = [[(0,0), (0,1), (1,1)], 
-                [(0,0),(1,0),(1,1)],
-                [(0,0), (0,1), (-1,1)],
-                [(0,0),(-1,0),(-1,1)],
-                [(0,0), (1,0), (2,0)],
-                [(0,0), (0,1),(0,2)]]
+shapes = [[[1, 0, 0],
+          [1, 1, 0],
+          [0, 0, 0]],
 
-max_sum = 0
+          [[1, 1, 0],
+          [0, 1, 0],
+          [0, 0, 0]],
+
+          [[0, 1, 0],
+          [1, 1, 0],
+          [0, 0, 0]],
+
+          [[1, 1, 0],
+          [1, 0, 0],
+          [0, 0, 0]],
+
+          [[1, 1, 1],
+          [0, 0, 0],
+          [0, 0, 0]],
+
+          [[1, 0, 0],
+          [1, 0, 0],
+          [1, 0, 0]],
+]
+
+def get_max_sum(x,y) :
+    max_sum = 0
+    for i in range(len(shapes)) :
+        if_possible = True
+        sum_of_nums = 0
+        for dy in range(3):
+            for dx in range(3) :
+                if shapes[i][dy][dx] == 0 :
+                    continue
+                if x + dx >= m or y + dy >= n :
+                    if_possible = False
+                else :
+                    sum_of_nums += grid[y + dy][x + dx]
+                
+        if if_possible :
+            max_sum = max(max_sum, sum_of_nums)
+
+    return max_sum
+
+ans = 0
 for i in range(n) :
     for j in range(m) :
-        # 첫번째 블럭
-        for block in type_shape :
-            if not is_in_range(j,i, block) : # 범위안에 포함되어있지 않다면, 
-                continue                         # 패스
-            
-            sum_num = 0
-            for elem in block :
-                dx, dy = elem
-                sum_num += grid[i+dy][j+dx]
+        ans = max(ans, get_max_sum(j,i))
 
-            max_sum = max(max_sum, sum_num)
-        
-print(max_sum)
+print(ans)
