967f72ac7fe0c1d23e3dcf21ee9fda120ab57be7
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-02-26 11:58:56 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 826ms, Memory: 29MB, Status: Passed - CodeTree

--- 240226/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240226/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,49 +1,80 @@
 import sys
-MIN_INT = -sys.maxsize
+INT_MIN = -sys.maxsize
 
 n,m = tuple(map(int, input().split()))
-
 grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
+board = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
 
-def in_range(x,y) :
-    return x >= 0 and x < n and y >= 0 and y < m
+def clear_board():
+    for i in range(n):
+        for j in range(m):
+            board[i][j] = 0
 
-def is_Overlap(x1, y1, x2, y2, h1, w1, h2, w2):
-    for x in range(x2, x2 + h2) :
-        for y in range(y2, y2 + w2) :
-            if x >= x1 and x < x1 + h1 and y >= y1 and y < y1 + w1 :
+
+def draw(x1, y1, x2, y2):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            board[i][j] += 1
+
+
+def check_board():
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] >= 2:
                 return True
-
     return False
 
 
-def get_maxScore(x, y, height, width) :
+def overlapped(x1, y1, x2, y2, x3, y3, x4, y4):
+    clear_board()
+    draw(x1, y1, x2, y2)
+    draw(x3, y3, x4, y4)
+    return check_board()
+
+
+def rect_sum(x1, y1, x2, y2):
     return sum([
         grid[i][j]
-        for i in range(x, x + height)
-        for j in range(y, y + width)
+        for i in range(x1, x2 + 1)
+        for j in range(y1, y2 + 1)
     ])
 
-ans = MIN_INT
-# 왼쪽 제일 위의 좌표를 잡아주고
-for x1 in range(n) :
-    for y1 in range(m) :
-        for h1 in range(1, n+1) :
-            for w1 in range(1, m+1) :
-                if in_range(x1 + h1 - 1, y1 + w1 - 1) :
-                    for x2 in range(n) :
-                        for y2 in range(m) :
-                            for h2 in range(1, n+1) :
-                                    for w2 in range(1,m+1) :
-                                        if not in_range(x2 + h2 - 1, y2 + w2 - 1) :
-                                            continue
 
-                                        if is_Overlap(x1, y1, x2, y2, h1, w1, h2, w2) :
-                                            continue
+def find_max_sum_with_rect(x1, y1, x2, y2):
+    max_sum = INT_MIN
+    
+    # (i, j), (k, l)을 양쪽 꼭지점으로 하는
+    # 두 번째 직사각형을 정하여
+    # 겹치지 않았을 때 중
+    # 최댓값을 찾아 반환합니다.
+    for i in range(n):
+        for j in range(m):
+            for k in range(i, n):
+                for l in range(j, m):
+                    if not overlapped(x1, y1, x2, y2, i, j, k, l):
+                        max_sum = max(max_sum, 
+                                      rect_sum(x1, y1, x2, y2) +
+                                      rect_sum(i, j, k, l))
+    
+    return max_sum
 
-                                        ans = max(ans, get_maxScore(x1, y1, h1, w1) + get_maxScore(x2, y2, h2, w2))
 
+def find_max_sum():
+    max_sum = INT_MIN
+
+    for i in range(n):
+        for j in range(m):
+            for k in range(i,n):
+                for l in range(j,m):
+                    max_sum = max(max_sum, find_max_sum_with_rect(i, j, k, l))
+    return max_sum
+
+
+ans = find_max_sum()
 print(ans)
