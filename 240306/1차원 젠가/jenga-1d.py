8bba207d413e2c7f870b5b0425cda2688a2ea04a
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-03-06 09:25:14 +0900

    Title: 1차원 젠가, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240306/1차원 젠가/jenga-1d.py
@@ -0,0 +1,26 @@
+n = int(input())
+Jenga_arr = [int(input()) for _ in range(n)]
+Jenga_arr.reverse()
+s1, e1 = tuple(map(int, input().split()))
+s2, e2 = tuple(map(int, input().split()))
+
+
+def remove_PartRange_anc_Copy(s, e):
+    global Jenga_arr
+    temp_arr = []
+    for i in range(len(Jenga_arr)) :
+        if i >= s and i <= e :
+            continue
+        temp_arr.append(Jenga_arr[i])
+
+    # 배열 복사
+    Jenga_arr = []
+    for i in range(len(temp_arr)):
+        Jenga_arr.append(temp_arr[i])
+
+remove_PartRange_anc_Copy(s1-1, e1-1)
+remove_PartRange_anc_Copy(s2-1, e2-1)
+
+print(len(Jenga_arr))
+for elem in Jenga_arr[::-1] :
+    print(elem)
