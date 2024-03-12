298103112d054b2c469a916ee475c84103ad1dcc
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-03-12 11:07:25 +0900

    Title: 2차원 폭발 게임, Time: 325ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240312/2차원 폭발 게임/The-2D-bomb-game.py
+++ 240312/2차원 폭발 게임/The-2D-bomb-game.py
@@ -7,7 +7,6 @@ grid = [
 temp = [ [0]*n for _ in range(n)]
 isChanged = False
 
-
 def Bomb():
     global isChanged
     isChanged = False
@@ -15,18 +14,23 @@ def Bomb():
         target_index = n-1
         cnt = 0
         for j in range(n-1, -1, -1):  
+            if grid[target_index][i] == 0:
+                target_index = j
+                cnt = 1
+                continue
+
             if grid[target_index][i] == grid[j][i]:
                 cnt += 1
 
                 if j == 0 and cnt >= m:
                     for k in range(target_index, target_index - cnt, -1):
                         grid[k][i] = 0
-                        isChanged = True
+                    isChanged = True
             else : 
                 if cnt >= m:
                     for k in range(target_index, target_index - cnt, -1):
                         grid[k][i] = 0
-                        isChanged = True
+                    isChanged = True
 
                 target_index = j
                 cnt = 1
@@ -80,11 +84,9 @@ for _ in range(k):
     Rotate()
 
 # 일단 터트려 보고, 
-while True:
+while isChanged:
     Bomb()
     #터트리기 전 상태와 터트린 후의 상태가 같다면 Stop
-    if isChanged:
-        break
 
 ans = 0
 for i in range(n):
