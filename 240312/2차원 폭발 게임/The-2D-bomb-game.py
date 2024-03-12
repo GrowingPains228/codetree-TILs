a0c6f5c5d05c5e702f954cb995c0efd59d409544
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-03-12 10:37:43 +0900

    Title: 2차원 폭발 게임, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240312/2차원 폭발 게임/The-2D-bomb-game.py
@@ -0,0 +1,85 @@
+n,m,k = tuple(map(int, input().split()))
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+temp = [ [0]*n for _ in range(n)]
+
+
+def Bomb():
+    for i in range(n) :
+        target_index = n-1
+        cnt = 1
+        for j in range(n-2, -1, -1):
+            if grid[target_index][i] == grid[j][i]:
+                cnt += 1
+
+                if j == 0 and cnt >= m:
+                    for k in range(target_index, target_index - cnt, -1):
+                        grid[k][i] = 0
+            else : 
+                if cnt >= m:
+                    for k in range(target_index, target_index - cnt, -1):
+                        grid[k][i] = 0
+
+                target_index = j
+                cnt = 1
+
+    Apply_Grabity()
+
+# 90도 돌려주기
+def Rotate():
+    temp = [
+        [grid[n-1-i][j] for i in range(n)]
+        for j in range(n)
+    ]
+
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = temp[i][j]
+
+    Apply_Grabity()
+
+
+
+# 중력 작용
+def Apply_Grabity():
+    temp = [ [0]*n for _ in range(n) ]
+
+    for i in range(n):
+        index = n-1
+        for j in range(n-1,-1, -1):
+            if grid[j][i] == 0:
+                continue
+            
+            temp[index][i] = grid[j][i]
+            index -= 1
+
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = temp[i][j]
+
+
+# 디버그를 위한 프린트 함수
+def Print_Grid(log):
+    print(log)
+    for row in grid:
+        for elem in row:
+            print(elem, end = " ")
+        print()
+
+
+for _ in range(k):
+    Bomb()
+    Rotate()
+    Bomb()
+
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] > 0 :
+            ans += 1
+
+print(ans)
