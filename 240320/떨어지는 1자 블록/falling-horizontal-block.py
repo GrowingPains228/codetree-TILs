13fd6f20de3e3d607e2f751c6f5aa2a52eed6321
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-03-20 10:59:19 +0900

    Title: 떨어지는 1자 블록, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240320/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240320/떨어지는 1자 블록/falling-horizontal-block.py
@@ -4,6 +4,7 @@ grid = [
     for _ in range(n)
 ]
 
+# 밑에서 부터 1이 채워져 있다고 가정하고 짠 함수 (* 근데, 테스트 케이스는 그게 아니었음)
 def get_HightestRow(start_colume, max_colume):
     Index_row = n-1
 
@@ -16,6 +17,16 @@ def get_HightestRow(start_colume, max_colume):
     
     return Index_row
 
+# 범위 안에서 전부 탐색하며 제일 위쪽 열의 Index를 반환하는 함수
+def get_HightestRow2(start_colume, max_colume):
+    Index_row = n-1
+
+    for c in range(start_colume, max_colume + 1):
+        for r in range(n-1, 0, -1):
+            if grid[r][c] :
+                Index_row = min(Index_row, r)
+    
+    return Index_row-1
 
 
 def fill_Entity(x, y, length):
@@ -25,7 +36,7 @@ def fill_Entity(x, y, length):
 
 def simulation():
     global m, k
-    height = get_HightestRow(k-1, k + m - 2)
+    height = get_HightestRow2(k-1, k + m - 2)
     fill_Entity(height, k-1, m)
     
 
