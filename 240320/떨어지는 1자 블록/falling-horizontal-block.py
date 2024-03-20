698b28b2fd483d8737780bd9a0d48aa5cdbd47dd
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-03-20 10:25:28 +0900

    Title: 떨어지는 1자 블록, Time: 128ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240320/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,38 @@
+n, m, k = map(int, input().split())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+def get_HightestRow(start_colume, max_colume):
+    Index_colum = start_colume
+    Index_row = n-1
+
+    for c in range(start_colume, max_colume + 1):
+        while True:
+            if not grid[Index_row][Index_colum] :
+                break
+
+            Index_row -= 1
+    
+    return Index_row
+
+
+
+def fill_Entity(x, y, length):
+    for i in range(y, y + length):
+        grid[x][i] = 1
+
+
+def simulation():
+    global m, k
+    height = get_HightestRow(k-1, k + m - 2)
+    fill_Entity(height, k-1, m)
+    
+
+simulation()
+
+for row in grid:
+    for elem in row :
+        print(elem, end = ' ')
+    print()
