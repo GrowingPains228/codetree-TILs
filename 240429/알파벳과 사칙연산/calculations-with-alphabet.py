b1641eed3700188e87a032fe31d0faba22c11346
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-04-29 17:30:46 +0900

    Title: 알파벳과 사칙연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240429/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,47 @@
+string_arr = list(input())
+string_length = len(string_arr)
+string_mapper = {
+    'a' : 0,
+    'b' : 1,
+    'c' : 2,
+    'd' : 3,
+    'e' : 4,
+    'f' : 5
+}
+map_arr = [0]*5
+ans = 0
+
+def calculate():
+    idx1 = string_mapper[string_arr[0]]
+    result = map_arr[idx1]
+
+    for i in range(1,string_length,2) :
+        (op, str2) = string_arr[i], string_arr[i+1]
+        idx = string_mapper[str2]
+        target = map_arr[idx]
+        result = operations(result, op, target)
+
+    return result
+
+
+def operations(result, op, target):
+    if op == '+':
+        return result + target
+    elif op == '-':
+        return result - target
+    else:
+        return result * target
+
+
+def Recursion(num):
+    global ans
+    if num == 5:
+        ans = max(ans, calculate())
+        return
+
+    for i in range(1,5):
+        map_arr[num] = i
+        Recursion(num+1)
+
+Recursion(0)
+print(ans)
