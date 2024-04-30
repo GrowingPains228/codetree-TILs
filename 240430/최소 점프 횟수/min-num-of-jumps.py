6a3c9afc74c784d23078553b91404a8e7ddb0682
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-04-30 17:02:53 +0900

    Title: 최소 점프 횟수, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240430/최소 점프 횟수/min-num-of-jumps.py
+++ 240430/최소 점프 횟수/min-num-of-jumps.py
@@ -4,17 +4,13 @@ ans = -1
 
 def recursion_Jump(idx, cnt):
     global ans
-    if idx == n-1: # 끝 지점에 도착 했으면
+
+    if idx >= n-1: # 끝 지점에 도착 했으면
         ans = cnt if ans == -1 else min(ans, cnt)
         return
 
-    # 끝에 도착 안했는데, 발판이 0이면 도달 못하는 것.
-    if jump_arr[idx] == 0:
-        return
-
     for i in range(1, jump_arr[idx]+1):
-        if idx+i < n:
-            recursion_Jump(idx+i, cnt + 1)
+        recursion_Jump(idx+i, cnt + 1)
 
 
 recursion_Jump(0, 0)
