793349e2352cd7754d7e930f70cd6a816fa376aa
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-02 18:52:10 +0900

    Title: n개의 점 중 m개 고르기, Time: 408ms, Memory: 28MB, Status: Passed - CodeTree

--- 240502/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240502/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -1,6 +1,6 @@
 import sys
 n, m = tuple(map(int, input().split()))
-dot_list= list()
+dot_list = list()
 select_list = list()
 for _ in range(n):
     x,y = tuple(map(int, input().split()))
@@ -8,44 +8,34 @@ for _ in range(n):
 ans = sys.maxsize
 
 
-def max_dist():
-    # (0,0)에서 가장 가깝고 가장 먼 점을 선택하면 된다.
-    min_dot, max_dot = (100,100),(1, 1)
-    min_dist, max_dist = (100**2 + 100**2), 2
+def Get_Distance():
+    dist = 0
 
-    for (x,y) in select_list:
-        dist = x**2 + y**2
-        if min_dist > dist :
-            min_dot = (x,y)
-            min_dist = dist
+    for i in range(m-1):
+        x1, y1 = select_list[i]
+        for j in range(i+1, m):
+            x2, y2 = select_list[j]
 
-        if max_dist < dist :
-            max_dot = (x,y)
-            max_dist = dist
+            dist = max(dist, (x1-x2)**2 + (y1-y2)**2)
 
-    return Cal_distance(min_dot, max_dot)
-
-
-def Cal_distance(dot1, dot2):
-    (x1, y1), (x2, y2) = dot1, dot2
-    return ((x1-x2)**2 + (y1-y2)**2)**(1/2)
+    return dist
 
 
 def find_max_dist(curr_idx, cnt):
-    global ans
+    global ans, select_list
     if curr_idx == n:
         if cnt == m:
-            ans = min(ans, max_dist())
+            ans = min(ans, Get_Distance())
         return
 
     # 골랐을때
     select_list.append(dot_list[curr_idx])
-    find_max_dist(curr_idx + 1, cnt + 1)
+    find_max_dist(curr_idx+1, cnt +1)
     select_list.pop()
 
     # 안골랐을때
-    find_max_dist(curr_idx + 1, cnt)
+    find_max_dist(curr_idx+1, cnt)
 
 
 find_max_dist(0,0)
-print(int(ans**2))
+print(int(ans))
