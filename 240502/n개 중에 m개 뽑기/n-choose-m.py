1301585ee88b2d181e05183e8988de9e6c60723d
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-02 10:33:19 +0900

    Title: n개 중에 m개 뽑기, Time: 214ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240502/n개 중에 m개 뽑기/n-choose-m.py
@@ -0,0 +1,23 @@
+N, M = tuple(map(int, input().split()))
+ans = []
+
+
+def print_all():
+    for elem in ans :
+        print(elem, end =' ')
+    print()
+
+
+def Choose(curr_num, value):
+    global ans
+    if curr_num == M:
+        print_all()
+        return
+
+    for i in range(value, N + 1):
+        if curr_num == 0 or ans[-1] < i:
+            ans.append(i)
+            Choose(curr_num+1, i+1)
+            ans.pop()
+
+Choose(0, 1)
