e919e2aab0cff5e20089058f947364ba9c14a5e6
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-03 11:22:48 +0900

    Title: 외판원 순회, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240503/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+visited = [False] * n
+path_arr = []
+ans = sys.maxsize
+
+
+def move_go(curr_num, curr_idx, path_cnt):
+    global ans
+    if curr_num == n-1:
+        ans = min(ans, path_cnt + grid[curr_idx][0])
+        return
+
+    for i in range(1, n):
+        if visited[i]:
+            continue
+
+        visited[i] = True
+        move_go(curr_num + 1, i, path_cnt + grid[curr_idx][i])
+        visited[i] = False
+
+
+visited[0] = True
+move_go(0,0,0)
+print(ans)
