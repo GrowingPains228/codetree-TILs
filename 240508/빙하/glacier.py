e6ca7f94bc0cf53ee7905e4957ce3d9665104062
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-08 15:07:46 +0900

    Title: 빙하, Time: 518ms, Memory: 29MB, Status: Passed - CodeTree

--- 240508/빙하/glacier.py
+++ 240508/빙하/glacier.py
@@ -1,15 +1,23 @@
 from collections import deque
+import enum
+
+
+class Element(enum.Enum):
+    WATER = 0
+    GLACIER = 1
+
+
 n, m = tuple(map(int, input().split()))
 grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 visited = [[False] * m for _ in range(n)]
-ans = 0
-Ice_arr = list()
+Ice_arr = deque()
 bfs_queue = deque()
-ans_time = 0
-ans_Ice = 0
+elapsed_time = 0
+last_melt_cnt = 0
+
 
 # bfs 를 통해 빙하를 판별할때,
 # 접근하려고 했지만 접근하지 못한 vertex에 대해 기억해 뒀다가,
@@ -33,18 +41,14 @@ def initialize():
 
 
 def can_go(x, y):
-    if not in_range(x,y):
-        return False
+    return in_range(x,y) and grid[x][y] == Element.WATER.value and not visited[x][y]
 
-    if visited[x][y]:
-        return False
 
-    if grid[x][y] == 1:
-        Ice_arr.append((x,y))
-        visited[x][y] = True
-        return False
-
-    return True
+# 범위를 벗어나지 않으면서 빙하여야 하고 이미
+# 선택된 적이 없어야 중복 없이 녹아야할 빙하 목록에
+# 해당 빙하를 문제 없이 추가할 수 있다.
+def is_glacier(x,y):
+    return in_range(x,y) and grid[x][y] == Element.GLACIER.value and not visited[x][y]
 
 
 def bfs():
@@ -56,28 +60,44 @@ def bfs():
             if can_go(nx,ny):
                 visited[nx][ny] = True
                 bfs_queue.append((nx, ny))
+            elif is_glacier(nx,ny):
+                Ice_arr.append((nx,ny))
+                visited[nx][ny] = True
 
 
 def Melt():
-    global ans_Ice
-    ans_Ice = len(Ice_arr)
     while Ice_arr:
-        x,y = Ice_arr.pop()
-        grid[x][y] = 0
+        x,y = Ice_arr.popleft()
+        grid[x][y] = Element.WATER.value
 
 
 def simulate():
-    global ans_time
+    global elapsed_time, last_melt_cnt, bfs_queue
 
-    while True:
-        initialize()
-        bfs()
-        if len(Ice_arr) > 0:
-            ans_time += 1
-            Melt()
-        else:
-            break
+    bfs()
+
+    # 녹일 빙하가 없다면 종료
+    if not Ice_arr:
+        return False
+
+    elapsed_time += 1
+    last_melt_cnt = len(Ice_arr)
+
+    bfs_queue = Ice_arr.copy()
+
+    # 녹아야 할 빙하들을 녹여준다.
+    Melt()
+
+    return True
 
 
-simulate()
-print(ans_time, ans_Ice)
+bfs_queue.append((0,0))
+visited[0][0] = True
+
+while True:
+    is_glacier_exist = simulate()
+
+    if not is_glacier_exist:
+        break
+
+print(elapsed_time, last_melt_cnt)
