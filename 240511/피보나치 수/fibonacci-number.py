6858f6d29f2f3e263bdf45c8b27128bd53fd4bdc
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-11 00:10:28 +0900

    Title: 피보나치 수, Time: 156ms, Memory: 24MB, Status: Passed - CodeTree

--- 240511/피보나치 수/fibonacci-number.py
+++ 240511/피보나치 수/fibonacci-number.py
@@ -1,7 +1,6 @@
 n = int(input())
 memo = [-1] * (n+1)
 dp = [0] * (n+1)
-dp[1], dp[2] = 1,1
 
 
 def Fibbo_Memoization(n):
@@ -17,6 +16,11 @@ def Fibbo_Memoization(n):
 
 
 def Fibbo_Tabulation(n):
+    if n <= 2:
+        return 1
+
+    dp[1], dp[2] = 1,1
+
     for i in range(3, n+1):
         dp[i] = dp[i-1] + dp[i-2]
 
@@ -25,5 +29,5 @@ def Fibbo_Tabulation(n):
 
 #print(f"Fibbo by Memoization : {Fibbo_Memoization(n)}")
 #print(f"Fibbo by Tabulation : {Fibbo_Tabulation(n)}")
-Fibbo_Tabulation(n)
-print(dp[n])
+
+print(Fibbo_Tabulation(n))
