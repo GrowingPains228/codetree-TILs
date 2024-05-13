88bd4f687525d6de990059055f360322c44f9da5
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-14 00:54:42 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240514/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -0,0 +1,36 @@
+n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dp = [
+        [[] for _ in range(n)]
+        for _ in range(n)
+]
+
+
+def initialize():
+    dp[0][0].append(grid[0][0]) # 최대 요소를 넣고
+    dp[0][0].append(grid[0][0]) # 최소 요소를 넣고
+    for i in range(1,n):
+        dp[i][0].append(max(dp[i-1][0][0], grid[i][0]))
+        dp[i][0].append(min(dp[i-1][0][1], grid[i][0]))
+
+        dp[0][i].append(max(dp[0][i-1][0], grid[0][i]))
+        dp[0][i].append(min(dp[0][i-1][1], grid[0][i]))
+
+
+initialize()
+for i in range(1, n):
+    for j in range(1, n):
+        if (max(dp[i-1][j][0], grid[i][j]) - min(dp[i-1][j][1], grid[i][j])
+                >= max(dp[i][j-1][0], grid[i][j]) - min(dp[i][j-1][1], grid[i][j])):
+            dp[i][j].append(max(dp[i][j-1][0], grid[i][j]))
+            dp[i][j].append(min(dp[i][j-1][1], grid[i][j]))
+        else:
+            dp[i][j].append(max(dp[i-1][j][0], grid[i][j]))
+            dp[i][j].append(min(dp[i-1][j][1], grid[i][j]))
+
+
+print(dp[n-1][n-1][0] - dp[n-1][n-1][1])
