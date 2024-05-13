54799aa8ac4e63d5fa6c6608abcf0bef47050104
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-13 14:01:21 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240513/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 240513/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -17,12 +17,6 @@ def initialize():
 initialize()
 for i in range(1, n):
     for j in range(1,n):
-        if dp[i][j-1] <= grid[i][j] and dp[i-1][j] <= grid[i][j]:
-            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-        elif dp[i][j-1] >= grid[i][j] and dp[i-1][j] >= grid[i][j]:
-            dp[i][j] = grid[i][j]
-        elif grid[i][j] > dp[i-1][j] :
-            dp[i][j] = max(grid[i][j], dp[i][j-1])
-        else:
-            dp[i][j] = max(grid[i][j], dp[i-1][j])
+        dp[i][j] = min(grid[i][j], max(dp[i-1][j], dp[i][j-1]))
+
 print(dp[n-1][n-1])
