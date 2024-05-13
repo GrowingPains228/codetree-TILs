1503f0519ee4a4b61ad798d2ab104043bff23b37
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-13 10:26:33 +0900

    Title: 사각형 채우기 2, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240513/사각형 채우기 2/rectangle-fill-2.py
+++ 240513/사각형 채우기 2/rectangle-fill-2.py
@@ -1,5 +1,6 @@
 n = int(input())
 MAX_SIZE = 1000
+MOD = 10007
 dp = [0]* (MAX_SIZE+1)
 dp[0] = 1
 dp[1] = 1
@@ -8,4 +9,4 @@ dp[2] = 3
 for i in range(3,n+1):
     dp[i] = dp[i-1]+ 2*dp[i-2]
 
-print(dp[n])
+print(dp[n]%MOD)
