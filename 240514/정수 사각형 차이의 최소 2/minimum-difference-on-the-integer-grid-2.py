879e9cd1f956f256659435b9d536b954fde61505
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-14 01:29:16 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 131ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240514/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240514/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -5,32 +5,45 @@ grid = [
 ]
 
 dp = [
-        [[] for _ in range(n)]
-        for _ in range(n)
+        [0]*n for _ in range(n)
+]
+dp_max = [
+        [0]*n for _ in range(n)
+]
+dp_min = [
+        [0]*n for _ in range(n)
 ]
 
 
 def initialize():
-    dp[0][0].append(grid[0][0]) # 최대 요소를 넣고
-    dp[0][0].append(grid[0][0]) # 최소 요소를 넣고
-    for i in range(1,n):
-        dp[i][0].append(max(dp[i-1][0][0], grid[i][0]))
-        dp[i][0].append(min(dp[i-1][0][1], grid[i][0]))
+    dp_max[0][0] = grid[0][0]
+    dp_min[0][0] = grid[0][0]
 
-        dp[0][i].append(max(dp[0][i-1][0], grid[0][i]))
-        dp[0][i].append(min(dp[0][i-1][1], grid[0][i]))
+    for i in range(1, n):
+        dp_max[0][i] = max(dp_max[0][i-1], grid[0][i])
+        dp_min[0][i] = min(dp_min[0][i-1], grid[0][i])
+
+        dp_max[i][0] = max(dp_max[i - 1][0], grid[i][0])
+        dp_min[i][0] = min(dp_min[i - 1][0], grid[i][0])
 
 
 initialize()
 for i in range(1, n):
     for j in range(1, n):
-        if (max(dp[i-1][j][0], grid[i][j]) - min(dp[i-1][j][1], grid[i][j])
-                >= max(dp[i][j-1][0], grid[i][j]) - min(dp[i][j-1][1], grid[i][j])):
-            dp[i][j].append(max(dp[i][j-1][0], grid[i][j]))
-            dp[i][j].append(min(dp[i][j-1][1], grid[i][j]))
+        if (max(grid[i][j],dp_max[i][j-1]) - min(grid[i][j],dp_min[i][j-1])
+                > max(grid[i][j],dp_max[i-1][j]) - min(grid[i][j],dp_min[i-1][j])):
+            dp_max[i][j] = max(grid[i][j], dp_max[i-1][j])
+            dp_min[i][j] = min(grid[i][j], dp_min[i-1][j])
+        elif (max(grid[i][j],dp_max[i][j-1]) - min(grid[i][j],dp_min[i][j-1])
+                < max(grid[i][j],dp_max[i-1][j]) - min(grid[i][j],dp_min[i-1][j])):
+            dp_max[i][j] = max(grid[i][j], dp_max[i][j-1])
+            dp_min[i][j] = min(grid[i][j], dp_min[i][j-1])
         else:
-            dp[i][j].append(max(dp[i-1][j][0], grid[i][j]))
-            dp[i][j].append(min(dp[i-1][j][1], grid[i][j]))
+            if dp_max[i][j-1] > dp_max[i-1][j]:
+                dp_max[i][j] = max(grid[i][j], dp_max[i-1][j])
+                dp_min[i][j] = min(grid[i][j], dp_min[i - 1][j])
+            else:
+                dp_max[i][j] = max(grid[i][j], dp_max[i][j - 1])
+                dp_min[i][j] = min(grid[i][j], dp_min[i][j - 1])
 
-
-print(dp[n-1][n-1][0] - dp[n-1][n-1][1])
+print(dp_max[n-1][n-1] - dp_min[n-1][n-1])
