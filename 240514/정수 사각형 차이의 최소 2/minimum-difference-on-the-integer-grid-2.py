0c6b831ea60fe52feadbd0f7a1ee4933856d6f1b
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-14 18:01:07 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 168ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240514/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240514/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,66 +1,50 @@
 import sys
 
+MAX_K = 100
 INT_MAX = sys.maxsize
-MAX_R = 100
 
-# 입력 받기
 n = int(input())
 grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
-dp = [
-    [0]*n
-    for _ in range(n)
-]
-
+dp = [[0]*n for _ in range(n)]
 ans = INT_MAX
 
-
-def initailize() :
+#초기화 함수
+def initialize():
     for i in range(n):
         for j in range(n):
             dp[i][j] = INT_MAX
-    
-    # 시작점의 경우, grid 시작점으로 초기화
+
     dp[0][0] = grid[0][0]
 
-    # 최좌측 열 및 행의 초기값을 설정
     for i in range(1, n):
-        dp[0][i] = max(dp[0][i-1], grid[0][i])
-        dp[i][0] = max(dp[i-1][0], grid[i][0])
+        dp[0][i] = max(dp[0][i-1],grid[0][i])
+        dp[i][0] = max(dp[i-1][0],grid[i][0])
 
 
-def solve(lower_bound):
-    #lower_bound 미만의 값은 사용할 수 없도록
-    #grid 값을 번경해준다.
+def sol(lower_bound):
     for i in range(n):
         for j in range(n):
             if grid[i][j] < lower_bound:
                 grid[i][j] = INT_MAX
     
-    # DP 초기화
-    initailize()
 
-    # 탐색하는 위치의 위에 값과 좌측 값 중에 작은 값과
-    # 해당 위치의 숫자 중에 최댓값을 구해준다.
     for i in range(1,n):
         for j in range(1,n):
-            dp[i][j] = max(min(dp[i-1][j], dp[i][j-1]), grid[i][j])
-    
+            dp[i][j] = max(grid[i][j], min(dp[i-1][j], dp[i][j-1]))
+
     return dp[n-1][n-1]
 
 
-# 최솟값을 k라고 가정했을 때
-# lower_bound 이상의 수들만 사용하여
-# 이동한다는 조건하에서
-# 최댓값을 최소로 만드는 DP문제를 풀어준다.
-for lower_bound in range(1, MAX_R+1):
-    upper_bound = solve(lower_bound)
+for lower_bound in range(1,MAX_K+1):
+    initialize()
+    upper_bound = sol(lower_bound)
 
     if upper_bound == INT_MAX:
         continue
-    
+
     ans = min(ans, upper_bound - lower_bound)
 
 print(ans)
