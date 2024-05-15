364f1dc29f1073a6dce99bfb89be1b8a7a1c8a45
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-15 23:08:11 +0900

    Title: 최대 감소 부분 수열, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240515/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 240515/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -1,13 +1,19 @@
-INT_MIN = -1
+import sys
+INT_MIN = -sys.maxsize
 MAX_VALUE = 10000
 
 n = int(input())
 arr = [MAX_VALUE+1] + list(map(int, input().split()))
-dp = [0]* (n+1)
+dp = [INT_MIN] * (MAX_VALUE+2)
+dp[MAX_VALUE+1] = 0
+for i in range(1, n+1):
+    j = arr[i]
 
-for i in range(n+1):
-    for j in range(i):
-        if arr[i] < arr[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+    for l in range(MAX_VALUE+1, j, -1):
+        if dp[l] == INT_MIN:
+            continue
+        
+        dp[j] = max(dp[j], dp[l]+1)
+
 
 print(max(dp))
