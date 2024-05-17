898c4f91401767c740d02fedf36c14bebadafdb2
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-17 17:47:09 +0900

    Title: 동전 거슬러주기, Time: 256ms, Memory: 26MB, Status: Passed - CodeTree

--- 240517/동전 거슬러주기/coin-change.py
+++ 240517/동전 거슬러주기/coin-change.py
@@ -1,35 +1,45 @@
-import sys
-sys.setrecursionlimit(10000)
-UNUSED = -1
-MAX_ANS = 10001
+from collections import deque
 
-n, m = tuple(map(int, input().split()))
-coin = [0] + list(map(int, input().split()))
+n,m = tuple(map(int, input().split()))
+coins = [0] + list(map(int, input().split()))
 
-memo = [UNUSED] * (m+1)
+q = deque()
+visited = [False] * (m+1)
 
-def find_min_cnt(sum):
-    if memo[sum] != UNUSED:
-        return memo[sum]
-    
-    # 합이 m이 되면 동전이 추가적으로 필요 없으므로
-    # 필요한 동전의 수 0을 반환한다.
-    if sum == m:
-        memo[sum] = 0
-        return 0
-
-    ans = MAX_ANS
-    for i in range(1, n+1):
-        if sum + coin[i] <= m:
-            ans = min(ans, find_min_cnt(sum + coin[i]) + 1)
-        
-    memo[sum] = ans
-    return ans
+step = [0] * (m+1)
+ans = 0
 
 
-ans_sum = find_min_cnt(0)
+def in_range(num):
+    return num <= m
 
-if ans_sum == MAX_ANS:
-    print(-1)
-else:
-    print(ans_sum)
+
+def can_go(num):
+    return in_range(num) and not  visited[num]
+
+
+def push(num, value):
+    visited[num] = True
+    step[num] = value
+    q.append(num)
+
+
+def find_min_cnt():
+    global ans
+
+    while q:
+        curr_num = q.popleft()
+
+        for i in range(1,n + 1):
+            if can_go(curr_num + coins[i]):
+                push(curr_num + coins[i], step[curr_num]+1)
+
+    if visited[m]:
+        ans = step[m]
+    else:
+        ans = -1
+
+
+push(0, 0)
+find_min_cnt()
+print(ans)
