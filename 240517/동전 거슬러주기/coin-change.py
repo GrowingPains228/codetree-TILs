e36c8d6e05b6da7539893511acced2abef49f41e
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-17 17:32:41 +0900

    Title: 동전 거슬러주기, Time: 282ms, Memory: 36MB, Status: Passed - CodeTree

--- 240517/동전 거슬러주기/coin-change.py
+++ 240517/동전 거슬러주기/coin-change.py
@@ -1,13 +1,15 @@
 import sys
-INT_MAX = sys.maxsize
+sys.setrecursionlimit(10000)
+UNUSED = -1
+MAX_ANS = 10001
 
 n, m = tuple(map(int, input().split()))
 coin = [0] + list(map(int, input().split()))
 
-memo = [INT_MAX] * (m+1)
+memo = [UNUSED] * (m+1)
 
 def find_min_cnt(sum):
-    if memo[sum] != INT_MAX:
+    if memo[sum] != UNUSED:
         return memo[sum]
     
     # 합이 m이 되면 동전이 추가적으로 필요 없으므로
@@ -16,7 +18,7 @@ def find_min_cnt(sum):
         memo[sum] = 0
         return 0
 
-    ans = INT_MAX
+    ans = MAX_ANS
     for i in range(1, n+1):
         if sum + coin[i] <= m:
             ans = min(ans, find_min_cnt(sum + coin[i]) + 1)
@@ -27,7 +29,7 @@ def find_min_cnt(sum):
 
 ans_sum = find_min_cnt(0)
 
-if ans_sum == INT_MAX:
+if ans_sum == MAX_ANS:
     print(-1)
 else:
     print(ans_sum)
