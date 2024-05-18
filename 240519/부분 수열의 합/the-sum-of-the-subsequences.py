251263e12107bf83b04023f16cc59d233c05abd0
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-19 03:44:58 +0900

    Title: 부분 수열의 합, Time: 156ms, Memory: 31MB, Status: Passed - CodeTree

--- 240519/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 240519/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -1,30 +1,27 @@
-import sys
-MAX_VALUE = 10000
-
 n, m = tuple(map(int, input().split()))
 arr = [0] + list(map(int, input().split()))
-arr.sort()
 
-if sum(arr) < m or min(arr) > m:
-    print("No")
-    sys.exit()
+# 지금까지 i번째 수까지만 고려해봤을 때,
+# 고른 수의 합을 j로 만드는 것이 가능하면 True
+# 불가능하면 False
+# dp[i][j]
+dp = [
+    [False] * (m+1)
+    for _ in range(n+1)
+]
 
-dp = [-1] * (MAX_VALUE+1)
-dp[0] = 0
-
+dp[0][0] = True
 
 for i in range(1, n+1):
-    for j in range(MAX_VALUE, -1, -1):
-        if j < arr[i]:
-            continue
+    for j in range(m+1):
+        if j >= arr[i] and dp[i-1][j - arr[i]]:
+            dp[i][j] = True
         
-        if dp[j - arr[i]] == -1:
-            continue
+        if dp[i-1][j]:
+            dp[i][j] = True
         
-        dp[j] = 1
 
-
-if dp[m] == -1:
-    print("No")
-else:
+if dp[n][m]:
     print("Yes")
+else:
+    print("No")
