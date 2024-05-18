524a0bd0c1478b89e1f66e15cad5b4d1fc118661
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-19 02:51:44 +0900

    Title: 부분 수열의 합, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240519/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -0,0 +1,28 @@
+import sys
+MAX_VALUE = 10000
+
+n, m = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
+
+if sum(arr) < m or min(arr) > m:
+    print("No")
+    sys.exit()
+
+dp = [-1] * (MAX_VALUE+1)
+dp[0] = 0
+
+
+for i in range(1, m+1):
+    for j in range(n+1):
+        if i < arr[j]:
+            continue
+
+        if dp[i] == -1:
+            continue
+        
+        dp[i] = dp[i - arr[j]] + 1
+
+if dp[m]:
+    print("Yes")
+else:
+    print("No")
