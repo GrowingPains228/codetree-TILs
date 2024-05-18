715ba6f64c5121fd34c1086d851f3e7086916663
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-18 19:07:27 +0900

    Title: 최대 동전 거슬러주기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240518/최대 동전 거슬러주기/max-coin-change.py
@@ -0,0 +1,25 @@
+import sys
+MAX_VALUE = -1
+n,m  = tuple(map(int,input().split()))
+arr = [0]+ list(map(int,input().split()))
+dp=[0]*(m+1)
+
+def initialize():
+    for i in range(m+1):
+        dp[i] = MAX_VALUE
+
+    dp[0] = 0
+
+
+initialize()
+for i in range(1, m+1):
+    for j in range(n):
+        if i >= arr[j]:
+            if dp[i-arr[j]] == MAX_VALUE:
+                continue
+
+            dp[i]=max(dp[i],dp[i-arr[j]]+1)
+if dp[m]==MAX_VALUE:
+    print(-1)
+else :
+    print(dp[m])
