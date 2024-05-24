ebfa30d956b8405392a62852e7092ff0949fdf2c
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-24 15:43:53 +0900

    Title: 올바른 등식 만들기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240524/올바른 등식 만들기/right-equality.py
+++ 240524/올바른 등식 만들기/right-equality.py
@@ -1,23 +1,28 @@
 MAX_RANGE = 20
 MIN_RANGE = -20
 n, m = tuple(map(int, input().split()))
-nums = [20] + list(map(int, input().split()))
+nums = list(map(int, input().split()))
 
 #  0 이상 40 이하로 있다고 조정한다.
 
 # dp[i][j]를 정의한다.
 # i번째 수를 (+)하거나 (-)했을때, 나온 결과값이 j일때, 최대 횟수의 값
 dp = [[0]*(MAX_RANGE*2+1) for _ in range(n+1)]
-dp[0][nums[0]] = 1
-for i in range(1, n+1):
-    for j in range(MAX_RANGE*2+1):
-        if j - nums[i] >= 0 and dp[i-1][j-nums[i]] != 0:
-            dp[i][j] = dp[i-1][j-nums[i]]
+dp[0][MAX_RANGE] += 1
 
-        if j + nums[i] <= MAX_RANGE*2 and dp[i-1][j+nums[i]] != 0:
-            if dp[i][j]:
-                dp[i][j] = max(dp[i-1][j+nums[i]], dp[i][j]) + 1
-            else:
-                dp[i][j] = dp[i-1][j+nums[i]]
+# n번째 전 까지 구한다.
+for i in range(n):
+    for j in range(MAX_RANGE*2+1):
+        if dp[i][j] == 0:
+            continue
+
+        if nums[i] == 0:
+            dp[i+1][j] = dp[i][j] * 2
+        else:
+            if j - nums[i] >= 0:
+                dp[i+1][j-nums[i]] = max(dp[i+1][j-nums[i]]+1, dp[i][j])
+
+            if j + nums[i] <= MAX_RANGE*2:
+                dp[i+1][j+nums[i]] = max(dp[i+1][j+nums[i]]+1, dp[i][j])
 
 print(dp[n][MAX_RANGE + m])
