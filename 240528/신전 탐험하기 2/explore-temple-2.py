427a625e6d5ce78522e1f5620478a5f08b07b6c2
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-05-28 08:49:53 +0900

    Title: 신전 탐험하기 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240528/신전 탐험하기 2/explore-temple-2.py
@@ -0,0 +1,35 @@
+n = int(input().rstrip())
+
+rooms = [[0, 0, 0]]
+
+for _ in range(n):
+    left, middle, right = tuple(map(int, input().rstrip().split()))
+    rooms.append([left, middle, right])
+
+# dp[i][j][k] : i방향룸을 택해서 j층에서 k방향 방으로 끝난 보물의 최대값
+dp = [[[0]*3 for _ in range(n+1)]for _ in range(3)]
+
+#left = 0, middle = 1, right = 2
+for i in range(3):
+    for j in range(3):
+        dp[i][1][j] = rooms[1][i]
+
+
+for i in range(3):
+    for j in range(2, n+1):
+        for k in range(3):
+            for z in range(3):
+                if k == z:
+                    continue
+                
+                dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][z] + rooms[j][k])
+
+ans = 0
+for i in range(3):
+    for j in range(3):
+        if i == j:
+            continue
+
+        ans = max(ans, dp[i][n][j])
+
+print(ans)
