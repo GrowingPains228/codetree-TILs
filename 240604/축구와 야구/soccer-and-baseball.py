9d5b066f4d3b23c248e45099c2d2fcbd6c1efa83
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-04 15:28:01 +0900

    Title: 축구와 야구, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240604/축구와 야구/soccer-and-baseball.py
+++ 240604/축구와 야구/soccer-and-baseball.py
@@ -5,17 +5,17 @@ for _ in range(n):
     players.append((s, b))
 
 # i번째 학생까지 선택했을때, 축구 j 명, 야구 k 명 선택했을 때의 최대 능력합
-dp = [[[0]*10 for _ in range(12)] for _ in range(n+1)]
+dp = [[[0]*(n+1) for _ in range(10)] for _ in range(12)]
 # 초기화
 soccer, baseball = players[1]
-dp[1][1][0] = soccer
-dp[1][0][1] = baseball
+dp[0][1][1] = baseball
+dp[1][0][1] = soccer
 
-for i in range(2, n+1):
-    s, b = players[i]
-    for j in range(1, 12):
-        for k in range(1, 10):
-            dp[i][j][k] = max(dp[i-1][j-1][k] + s, dp[i-1][j][k-1] + b)
+for i in range(1, 12):
+    for j in range(1, 10):
+        for k in range(2, n+1):
+            s, b = players[k]
 
+            dp[i][j][k] = max(dp[i-1][j][k-1] + s, dp[i][j-1][k-1] + b)
 
-print(dp[n][11][9])
+print(max(dp[11][9]))
