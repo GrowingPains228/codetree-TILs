42b19457f206a9c946963a84e09d70b38b889b5f
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-05 16:33:00 +0900

    Title: 세 수의 합, Time: 203ms, Memory: 25MB, Status: Passed - CodeTree

--- 240605/세 수의 합/sum-of-three-num.py
+++ 240605/세 수의 합/sum-of-three-num.py
@@ -9,21 +9,13 @@ for elem in arr:
     else:
         count[elem] = 1
 
-diff_index = []
-
 ans = 0
-for elem1 in arr:
-    count[elem1] -= 1
-    diff  = k - elem1
-    for elem2 in arr:
-        if count[elem2] <= 0:
-            continue
-        count[elem2] -= 1
-        diff_index.append(elem2)
-        if (diff - elem2) in count and count[diff - elem2] > 0 :
-            ans += count[diff - elem2]
-    
-    while len(diff_index) > 0:
-        count[diff_index.pop()] += 1
+for i in range(n):
+    count[arr[i]] -= 1
+    for j in range(i):
+        diff = k - arr[i] - arr[j]
+
+        if diff in count:
+            ans += count[diff]
 
 print(ans)
