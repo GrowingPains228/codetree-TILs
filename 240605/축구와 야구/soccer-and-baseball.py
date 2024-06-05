6a6c94d84429779723b2499f5558ea5e697dd544
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-05 11:00:25 +0900

    Title: 축구와 야구, Time: 172ms, Memory: 26MB, Status: Passed - CodeTree

--- 240605/축구와 야구/soccer-and-baseball.py
+++ 240605/축구와 야구/soccer-and-baseball.py
@@ -1,3 +1,4 @@
+import sys
 n = int(input())
 players = [(0,0)]
 players.sort()
@@ -6,17 +7,20 @@ for _ in range(n):
     players.append((s, b))
 
 # i번째 학생까지 선택했을때, 축구 j 명, 야구 k 명 선택했을 때의 최대 능력합
-dp = [[[0]*(10) for _ in range(12)] for _ in range(n+1)]
+dp = [[[-1]*(10) for _ in range(12)] for _ in range(n+1)]
 # 초기화
-
+dp[0][0][0] = 0
 for i in range(1, n+1):
-    s, b = players[i]
+    soccer, baseball = players[i]
     for j in range(min(i+1, 12)):
         for k in range(min(i+1, 10)):
+            if dp[i-1][j][k] != -1:
+                dp[i][j][k] = dp[i-1][j][k]
+
             if k > 0:
-                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k-1] + b)
+                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k-1] + baseball)
 
             if j > 0:
-                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j-1][k] + s)
+                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j-1][k] + soccer)
 
 print(dp[n][11][9])
