96bce918c08d2cf6f08f428528e9b80d2a97acdf
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-07 11:36:25 +0900

    Title: 특별한 문자, Time: 200ms, Memory: 25MB, Status: Passed - CodeTree

--- 240607/특별한 문자/special-character.py
+++ 240607/특별한 문자/special-character.py
@@ -1,14 +1,15 @@
 from collections import defaultdict
-inp = input()
+import sys
 
+inp = input()
 dic = defaultdict(int)
-ans_char = ''
 
 for i in range(len(inp)):
     dic[inp[i]] += 1
 
-for key, value in dic.items():
-    if value == 1 and ans_char == '':
-        ans_char = key
+for elem in inp:
+    if dic[elem] == 1:
+        print(elem)
+        sys.exit()
 
-print(ans_char if ans_char != '' else "None")
+print("None")
