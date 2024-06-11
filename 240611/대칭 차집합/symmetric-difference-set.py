9b0c4c258f988d23f665f7ab3ada21831ed87706
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-11 13:39:00 +0900

    Title: 대칭 차집합, Time: 204ms, Memory: 50MB, Status: Passed - CodeTree

--- 240611/대칭 차집합/symmetric-difference-set.py
+++ 240611/대칭 차집합/symmetric-difference-set.py
@@ -2,20 +2,11 @@ n1, n2 = tuple(map(int, input().split()))
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-s1 = set()
-s2 = set()
-for elem in A:
-    s1.add(elem)
-
-for elem in B:
-    s2.add(elem)
+s1 = set(A)
+ans = n1 + n2
 
 for elem in B:
     if elem in s1:
-        s1.remove(elem)
+        ans -= 2
 
-for elem in A:
-    if elem in s2:
-        s2.remove(elem)
-
-print(len(s1) + len(s2))
+print(ans)
