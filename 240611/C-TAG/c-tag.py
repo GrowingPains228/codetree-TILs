bf1a7878189b1aa51494f922cbd5fb5bd2ee2c89
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-11 09:20:46 +0900

    Title: C-TAG, Time: 1161ms, Memory: 26MB, Status: Passed - CodeTree

--- 240611/C-TAG/c-tag.py
+++ 240611/C-TAG/c-tag.py
@@ -1,24 +1,28 @@
 n,m = tuple(map(int, input().split()))
 list_A = [list(input()) for _ in range(n)]
 list_B = [list(input()) for _ in range(n)]
+
 ans = 0
+s = set()
+
+
+def test_location(x, y, z):
+    s.clear()
+
+    for i in range(n):
+        s.add(list_A[i][x] + list_A[i][y] + list_A[i][z])
+    
+    for i in range(n):
+        if (list_B[i][x] + list_B[i][y] + list_B[i][z]) in s:
+            return False
+    
+    return True
+
+
 for i in range(m-2):
     for j in range(i+1, m-1):
         for k in range(j+1, m):
-            sa, sb = set(), set()
-            for z in range(n):
-                string_a = list_A[z][i] + list_A[z][j] + list_A[z][k]
-                string_b = list_B[z][i] + list_B[z][j] + list_B[z][k]
-                sa.add(string_a)
-                sb.add(string_b)
-        
-            tf = True
-            for elem in sb:
-                if elem in sa:
-                    tf = False
-                    break
-
-            if tf:
+            if test_location(i, j, k):
                 ans += 1
 
 print(ans)
