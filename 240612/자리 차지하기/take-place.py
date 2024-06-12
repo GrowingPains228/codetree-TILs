3a796d4d9fb1873e8e5d6bb860e4c6bcaacbcc11
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-12 11:28:51 +0900

    Title: 자리 차지하기, Time: 1080ms, Memory: 39MB, Status: Passed - CodeTree

--- 240612/자리 차지하기/take-place.py
+++ 240612/자리 차지하기/take-place.py
@@ -4,17 +4,15 @@ arr = list(map(int, input().split()))
 from sortedcontainers import SortedSet
 
 # 비어 있는 의자들 세팅
-s = SortedSet()
-for i in range(1, m+1):
-    s.add(i)
+s = SortedSet(range(1, m+1))
 
 ans = 0
 for elem in arr:
     chair = s.bisect_right(elem)
-    if len(s) == 0 or chair - 1 < 0:
+    if chair == 0:
         break
-    else :
-        s.remove(s[chair-1])
-        ans += 1
+
+    s.remove(s[chair-1])
+    ans += 1
 
 print(ans)
