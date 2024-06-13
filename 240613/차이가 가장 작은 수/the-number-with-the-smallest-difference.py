9d75ae259516b8e9dc7faad01e69f658c6162eec
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-13 10:02:13 +0900

    Title: 차이가 가장 작은 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240613/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
@@ -0,0 +1,27 @@
+import sys
+from sortedcontainers import SortedSet
+MAX_RANGE = 10**9
+n, m = tuple(map(int, input().split()))
+arr = SortedSet()
+ans = sys.maxsize
+for _ in range(n):
+    num1 = int(input())
+    arr.add(num1)
+
+for i in range(n//2):
+    num1 = arr[i]
+    num2 = m + num1
+    idx = arr.bisect_right(num2)
+    if idx == n:
+        continue
+    
+    if arr[idx-1] == num2:
+        ans = m
+        break
+    else:
+        ans = min(ans, arr[idx]- num1)
+
+if ans == sys.maxsize:
+    print(-1)
+else:
+    print(ans)
