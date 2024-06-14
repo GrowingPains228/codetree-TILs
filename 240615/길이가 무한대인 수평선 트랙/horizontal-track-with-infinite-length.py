a0642340708fbaf7ed19520c8bc7f17578e7f942
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-15 02:31:03 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 5102ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240615/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
+++ 240615/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
@@ -7,12 +7,11 @@ for _ in range(N):
     players.add((-p,v))
 
 newPlayers = SortedSet()
-positionDict = dict()
-
+checkV = dict()
 for _ in range(T):
     length = len(players)
     newPlayers.clear()
-    positionDict.clear()
+    checkV.clear()
     for i in range(length):
         curr_p, velocity = players[i]
         findidx = newPlayers.bisect_right((-curr_p, _))
@@ -20,26 +19,22 @@ for _ in range(T):
         if findidx != len(newPlayers):
             position, new_velocity = newPlayers[findidx]
             if velocity >= new_velocity and position <= -curr_p + velocity:
-                curr_p = position
+                curr_p, velocity = position, new_velocity
             else:
                 curr_p = -curr_p + velocity
         else:
             curr_p = -curr_p + velocity
 
-        if curr_p in positionDict:
-            positionDict[curr_p] = min(positionDict[curr_p], velocity)
-        else:
-            positionDict[curr_p] = velocity
-
+        checkV[velocity] = 1
         newPlayers.add((curr_p, velocity))
+
     players.clear()
+    
+    for i in range(len(newPlayers)):
+        p,v = newPlayers[i]
+        players.add((-p,v))
 
-    for position, v in positionDict.items():
-        startidx = newPlayers.bisect_left((position, v))
-        (cur_pos, velocity) = newPlayers[startidx]
-        players.add((-cur_pos, velocity))
-
-    if len(players) == 1:
+    if len(players) == 1 or len(checkV) == 1:
         break
 
 print(len(players))
