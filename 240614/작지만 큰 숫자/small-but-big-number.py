e4c988f558ec7849970ac21ecf0c8274af853211
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-14 13:49:34 +0900

    Title: 작지만 큰 숫자, Time: 1036ms, Memory: 40MB, Status: Passed - CodeTree

--- 240614/작지만 큰 숫자/small-but-big-number.py
+++ 240614/작지만 큰 숫자/small-but-big-number.py
@@ -4,17 +4,11 @@ arr = SortedSet(list(map(int, input().split())))
 m_list = list(map(int, input().split()))
 
 for elem in m_list:
-    target = arr.bisect_left(elem)
-    if target == len(arr) : # 없는 경우
-        print(-1)
-        continue
+    target = arr.bisect_right(elem)
     
-    if target == 0 and arr[target] != elem:
+    if target == 0 :
         print(-1)
         continue
 
-    if arr[target] > elem:
-        target -= 1
-
-    print(arr[target])
-    arr.remove(arr[target])
+    print(arr[target-1])
+    arr.remove(arr[target-1])
