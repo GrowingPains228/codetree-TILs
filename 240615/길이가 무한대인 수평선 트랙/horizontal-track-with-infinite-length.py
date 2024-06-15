88c69477a82ce1ae2038933818b5884548327f88
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-15 19:37:11 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 4459ms, Memory: 59MB, Status: Passed - CodeTree

--- 240615/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
+++ 240615/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
@@ -1,65 +1,57 @@
 from sortedcontainers import SortedSet
 
-# 입력 받기
-N, T = tuple(map(int, input().split()))
-# TreeSet 정의
-players = SortedSet()
-for _ in range(N):
-    p, v = tuple(map(int, input().split()))
-    players.add((-p,v))
-
-# 임시 TreeSet 정의
-newPlayers = SortedSet()
-velocity_dic = set()
-
-# 시간이 자동으로 끝나는 조건을 확인한다.
-# 총 플레이어가 1명이 된 경우, 남아있는 그룹들의 속도가 전부 똑같은 경우, (위치, 속도)가 다르지만 오름차순인 경우
-# 
-def IsEnd():
-    length = len(players)
-    if length == 1:
-        return True
-
-    if len(velocity_dic) == 1:
-        return True
-
-    for i in range(1, length):
-        (p1, v1), (p2, v2) = players[i-1], players[i]
-        if v2 < v1 :
-            return False
-    
-    return True
+n, t = tuple(map(int, input().split()))
+p, v = list(), list()
 
 
-# T분 만큼 돌면서 실행해주기.
-for _ in range(T):
-    length = len(players)
-    newPlayers.clear()
-    velocity_dic.clear()
-    #시작복잡도 : NlogN
-    for i in range(length):
-        curr_p, velocity = players[i]
-        findidx = newPlayers.bisect_right((-curr_p, _))
+for _ in range(n):
+    x, velocity = tuple(map(int, input().split()))
+    p.append(x)
+    v.append(velocity)
 
-        if findidx != len(newPlayers):
-            position, new_velocity = newPlayers[findidx]
-            if velocity >= new_velocity and position <= -curr_p + velocity:
-                curr_p, velocity = position, new_velocity
-            else:
-                curr_p = -curr_p + velocity
-        else:
-            curr_p = -curr_p + velocity
+people_p = SortedSet()
+events = SortedSet()
 
-        newPlayers.add((curr_p, velocity))
-        velocity_dic.add(velocity)
 
-    players.clear()
-    #시작복잡도 : NlogN
-    for i in range(len(newPlayers)):
-        p,v = newPlayers[i]
-        players.add((-p,v))
+def add_event(p1, v1, p2, v2):
+    if v1 <= v2:
+        return
 
-    if IsEnd():
+    events.add(((p2-p1)/(v1-v2), p1, v1))
+
+
+def remove_event(p1, v1, p2, v2):
+    if v1 <= v2:
+        return
+
+    events.remove(((p2-p1)/(v1-v2), p1, v1))
+
+
+for i in range(n):
+    people_p.add((p[i], v[i]))
+
+for i in range(n-1):
+    add_event(p[i], v[i], p[i+1], v[i+1])
+
+while events:
+    curr_t, x, velocity = events[0]
+
+    if curr_t > t:
         break
+    
+    people_p.remove((x,velocity))
+    events.remove((curr_t, x, velocity))
 
-print(len(players))
+    index = people_p.bisect_right((x,velocity))
+    np, nv = people_p[index]
+
+    # 바로 뒤에 사람이 있다면
+    # 사람의 정보를 가져와서
+    # 업데이트 해준다.
+    if index != 0:
+        index -= 1
+        px, pv = people_p[index]
+        remove_event(px, pv, x, velocity)
+        add_event(px, pv, np, nv)
+    
+print(len(people_p))
