004aca858671a1162eb906825049c92d66378854
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-17 16:17:13 +0900

    Title: 앞에서부터 삭제하기 2, Time: 488ms, Memory: 42MB, Status: Passed - CodeTree

--- 240617/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240617/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,22 +1,24 @@
-# 오름차순으로 정렬된 배열에서
-# 앞에서부터 K+1를 삭제한 후,
-# 평균을 구하면 끝.
 import heapq
 import statistics
 
 n = int(input())
 arr = list(map(int, input().split()))
 
-ans = 0
-total = sum(arr)
-length = len(arr)
+sum_val = 0
+max_avg = 0
 pq = []
 
-for k in range(n-2, 0, -1):
-    diff = total - sum(arr[:k])
-    new_arr = sorted(arr[k::])
-    heapq.heappush(pq, (-(diff-new_arr[0])/(length-k-1)))
+# 가장 마지막 부터 탐색한다고 생각.
+heapq.heappush(pq, arr[n-1])
+sum_val += arr[n-1]
 
-ans = -pq[0]
+for i in range(n-2, 0, -1):
+    heapq.heappush(pq, arr[i])
+    sum_val += arr[i]
 
-print(f"{ans:0.2f}")
+    min_num = pq[0]
+    avg = (sum_val-min_num)/(n-i-1)
+
+    max_avg = max(max_avg, avg)
+
+print(f"{max_avg:.2f}")
