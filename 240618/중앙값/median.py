91c913ef0a8dca762230082ea7804ebb8fddfba0
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-18 10:27:52 +0900

    Title: 중앙값, Time: 645ms, Memory: 35MB, Status: Passed - CodeTree

--- 240618/중앙값/median.py
+++ 240618/중앙값/median.py
@@ -2,22 +2,30 @@ import heapq
 t = int(input())
 
 for _ in range(t):
-    cur_pop_max = []
     m = int(input())
     arr = list(map(int, input().split()))
-    pq = []
-    heapq.heappush(pq, arr[0])
-    print(pq[0], end = " ")
-    for i in range(1, m):
-        heapq.heappush(pq, arr[i])
-        if i%2 == 0:
-            popitem = heapq.heappop(pq)
-            heapq.heappush(cur_pop_max, -popitem)
-            if - cur_pop_max[0] > pq[0]:
-                tmp = heapq.heappop(pq)
-                heapq.heappush(pq, -heapq.heappop(cur_pop_max))
-                heapq.heappush(cur_pop_max, -tmp)
-                print(pq[0], end = " ")
+
+    max_pq = []
+    min_pq = []
+    median = arr[0]
+    print(median, end = " ")
+
+    for i in range(1,m):
+        if i % 2 == 1:
+            if arr[i] < median:
+                heapq.heappush(max_pq, -arr[i])
             else:
-                print(pq[0], end = " ")
+                heapq.heappush(min_pq, arr[i])
+        else:
+            if len(max_pq) > len(min_pq):
+                new_cand = -heapq.heappop(max_pq)
+            else:
+                new_cand = heapq.heappop(min_pq)
+
+            nums = sorted([median, arr[i], new_cand])
+            heapq.heappush(max_pq, -nums[0])
+            median = nums[1]
+            heapq.heappush(min_pq, nums[2])
+            print(median, end = " ")
+
     print()
