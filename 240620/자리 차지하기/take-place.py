7ddf8dc42dccfed61b700bc2595ae91b52832c68
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-06-20 20:21:57 +0900

    Title: 자리 차지하기, Time: 965ms, Memory: 38MB, Status: Passed - CodeTree

--- 240620/자리 차지하기/take-place.py
+++ 240620/자리 차지하기/take-place.py
@@ -4,15 +4,18 @@ n,m = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 s = SortedSet()
 
+for i in range(1, m+1):
+    s.add(i)
+
 ans = 0
 for elem in arr:
-    if elem == 1 and 1 in s:
+    idx = s.bisect_right(elem)
+
+    if idx -1 < 0:
         break
+    else:
+        ans += 1
+        s.remove(s[idx-1])
     
-    for i in range(elem):
-        if elem-i not in s:
-            s.add(elem-i)
-            ans += 1
-            break
 
 print(ans)
