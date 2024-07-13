a3f67222e5ba46950ef5d34d7e26099b865810f3
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-07-13 13:07:42 +0900

    Title: 부분 수열의 합이 k, Time: 164ms, Memory: 25MB, Status: Passed - CodeTree

--- 240713/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.py
+++ 240713/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.py
@@ -1,12 +1,21 @@
-n, k = tuple(map(int, input().split()))
+n,k = tuple(map(int, input().split()))
 arr = [0] + list(map(int, input().split()))
-
-sumArr = [[0] * (n+1) for _ in range(n+1)]
+prefix_sum  = [0] * (n+1)
 ans = 0
+
+
+def get_sum(s, e):
+    return prefix_sum[e] - prefix_sum[s-1]
+
+
+prefix_sum[0] = 0
+for i in range(1, n+1):
+    prefix_sum[i] = prefix_sum[i-1] + arr[i]
+
+
 for i in range(1, n+1):
     for j in range(i, n+1):
-        sumArr[i][j] = sumArr[i][j-1] + arr[j]
-        if sumArr[i][j] == k:
+        if get_sum(i, j) == k:
             ans += 1
 
 print(ans)
