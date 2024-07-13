1621e096d4eab1b91429f28430284aa4ce501f60
95_CYH_ClientProgmmer <150199521+GrowingPains228@users.noreply.github.com>
2024-07-13 13:24:51 +0900

    Title: 정수 n개의 합 3, Time: 155ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240713/정수 n개의 합 3/sum-of-n-integers-3.py
@@ -0,0 +1,13 @@
+MAP_RANGE = 100
+
+n, k = tuple(map(int, input().split()))
+myMap = [[0] * (n+1)] + [([0] + list(map(int, input().split()))) for _ in range(n)]
+
+sumArr = [[0] * (n+1) for _ in range(n+1)]
+ans = 0
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        sumArr[i][j] = myMap[i][j] + sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1]
+        ans = max(ans, sumArr[i][j] - sumArr[i-k][j] - sumArr[i][j-k] + sumArr[i-k][j-k])
+
+print(ans)
