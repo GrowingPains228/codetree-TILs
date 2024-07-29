e7424b816eca908b26ab0adcf47dabd7f02c8db8
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-07-29 12:59:05 +0900

    Title: 점 개수 세기, Time: 2417ms, Memory: 65MB, Status: Passed - CodeTree

--- 240729/점 개수 세기/count-number-of-points.py
+++ 240729/점 개수 세기/count-number-of-points.py
@@ -1,30 +1,33 @@
-from sortedcontainers import SortedDict
+from sortedcontainers import SortedSet
 
+# 변수 선언 및 입력:
 n, q = tuple(map(int, input().split()))
 dotList = list(map(int, input().split()))
-dotToIndex = dict()
-sm = SortedDict()
-for key in dotList:
-    sm[key] = True
+queries = [
+    tuple(map(int, input().split()))
+    for _ in range(q)
+]
 
-qList = list()
+mapper = {}
 
-# 단순히 질의에 대한 범위는 False로.
-for _ in range(q):
-    a, b = tuple(map(int, input().split()))
-    qList.append((a, b))
-    if a not in sm:
-        sm[a] = False
-    if b not in sm:
-        sm[b] = False
+# 주어진 x좌표값들을 전부 treeset에 넣어준다.
+nums = SortedSet(dotList)
 
-for index, key in enumerate(sm.keys(), start=1):
-    dotToIndex[key] = index
+# 예외를 처리하기 위해 최댓값보다 큰 값을 treeset에 넣어준다.
+nums.add((10**9) + 1)
 
-sumList = list(0 for _ in range(len(sm)+1))   # index 순으로 Sum 한 리스트!
-for index, key in enumerate(dotToIndex.keys(), start=1):
-    sumList[index] = sumList[index-1] + 1 if sm[key] else sumList[index-1]
+idx = 1
+for num in nums:
+    mapper[num] = idx
+    idx += 1
 
+# 질의에 대해
+# 각 [a,b] 에 해당하는 번호를
+# mapper를 통해 구해
+# 두 번호 사이의 점의 수를 출력한다.
 
-for (x, y) in qList:
-    print(sumList[dotToIndex[y]] - sumList[dotToIndex[x]-1])
+for a,b in queries:
+    new_a = mapper[nums[nums.bisect_left(a)]]
+    new_b = mapper[nums[nums.bisect_right(b)]]
+
+    print(new_b - new_a)
