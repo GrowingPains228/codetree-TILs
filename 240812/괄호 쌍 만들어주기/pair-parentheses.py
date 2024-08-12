2b0ca5fb65a9ec062a972958fb824bdaf998494e
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-08-12 23:54:19 +0900

    Title: 괄호 쌍 만들어주기, Time: 140ms, Memory: 27MB, Status: Passed - CodeTree

--- 240812/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240812/괄호 쌍 만들어주기/pair-parentheses.py
@@ -1,15 +1,7 @@
 string = list(input())
 length = len(string)
 
-R = [0] * length # )) 의 개수
 L = [0] * length # (( 의 개수
-
-for i in range(length-2, -1, -1):
-    if string[i] == ')' and string[i+1] == ')' :
-        R[i] = R[i+1] + 1
-    else:
-        R[i] = R[i+1]
-
 
 for i in range(1, length):
     if string[i] == '(' and string[i-1] == '(' :
@@ -19,6 +11,7 @@ for i in range(1, length):
 
 ans = 0
 for i in range(length-1):
-    ans = max(ans, L[i] * R[i+1])
+    if string[i] == ')' and string[i+1] == ')' :
+        ans += L[i]
 
 print(ans)
