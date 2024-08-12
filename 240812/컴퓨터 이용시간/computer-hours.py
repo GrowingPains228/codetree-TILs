4c53a5705c9b10f989d912e33a8607160793e3df
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-08-12 11:30:05 +0900

    Title: 컴퓨터 이용시간, Time: 1141ms, Memory: 51MB, Status: Passed - CodeTree

--- 240812/컴퓨터 이용시간/computer-hours.py
+++ 240812/컴퓨터 이용시간/computer-hours.py
@@ -1,41 +1,28 @@
-from sortedcontainers import SortedSet
-
-class Person:
-    def __init__(self, index):
-        self.Id = index
-
-    def setUsingComputer(self, usingCom):
-        self.usingCom = usingCom
-
-    def getUsingComputer(self):
-        return self.usingCom
-
+import heapq
 
 n = int(input())
-people = [Person(i) for i in range(0, n+1)]
-useList = [tuple(map(int, input().split())) for _ in range(n)]
-segments = list()
-for index, (p, q) in enumerate(useList, start=1):
-    segments.append((p, 1, index))
-    segments.append((q, -1, index))
 
-# PC 리스트 초기화
-pcList = SortedSet()
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+
+# 각 사람별로 할당된 컴퓨터 번호를 관리한다.
+assigned_nums = [0] * n
+
+computers = list()
 for i in range(1, n+1):
-    pcList.add(i)
+    heapq.heappush(computers, i)
 
-segments.sort()
-segs = set()
+points = list()
+for i, (a, b) in enumerate(segments):
+    points.append((a, 1, i))
+    points.append((b, -1, i))
 
-for time, v, index in segments:
+points.sort()
+
+for x, v, index in points:
     if v == 1:
-        # 컴퓨터 사용
-        people[index].setUsingComputer(pcList.pop(0))
-        segs.add(index)
+        assigned_nums[index] = heapq.heappop(computers)
     else:
-        segs.remove(index)
-        # 사용하던 컴퓨터 반환
-        pcList.add(people[index].getUsingComputer())
+        heapq.heappush(computers, assigned_nums[index])
 
-for i in range(1, n+1):
-    print(people[i].getUsingComputer(), end=' ')
+for elem in assigned_nums:
+    print(elem, end =' ')
