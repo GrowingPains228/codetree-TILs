4fb13370d4992176c00d704161c6273fd6f43524
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-08-14 10:23:15 +0900

    Title: 가장 짧은 부분합, Time: 189ms, Memory: 32MB, Status: Passed - CodeTree

--- 240814/가장 짧은 부분합/shortest-subtotal.py
+++ 240814/가장 짧은 부분합/shortest-subtotal.py
@@ -6,7 +6,7 @@ ans = sys.maxsize
 sum_val = 0
 j = 0
 for i in range(1, n):
-    while j + 1 < n :
+    while j + 1 <= n :
         if sum_val >= s:
             ans = min(ans, j - i + 1)
             break
