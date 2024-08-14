c5de20c90e6a9dd14d31395a97656b7e7d6f0ad3
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-08-14 10:36:28 +0900

    Title: 가장 짧은 부분합, Time: 145ms, Memory: 32MB, Status: Passed - CodeTree

--- 240814/가장 짧은 부분합/shortest-subtotal.py
+++ 240814/가장 짧은 부분합/shortest-subtotal.py
@@ -6,14 +6,14 @@ ans = sys.maxsize
 sum_val = 0
 j = 0
 for i in range(1, n):
-    while j + 1 <= n :
-        if sum_val >= s:
-            ans = min(ans, j - i + 1)
-            break
-
+    while j + 1 <= n and sum_val < s:
         sum_val += arr[j+1]
         j += 1
 
+    if sum_val < s:
+        break
+
+    ans = min(ans, j - i + 1)
     sum_val -= arr[i]
 
 if ans == sys.maxsize:
