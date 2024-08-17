a838cbe44cd45962ee60186b1870495c8806a010
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-08-17 23:48:58 +0900

    Title: 0에 가장 가까운 합, Time: 160ms, Memory: 31MB, Status: Passed - CodeTree

--- 240817/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 240817/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -6,13 +6,11 @@ arr.sort()
 
 left, right = 0, n-1
 ans = sys.maxsize
-ans = min(ans, abs(arr[right] + arr[left]))
 while left < right:
-    if arr[right] - arr[left] >= 0:
+    ans = min(ans, abs(arr[right] + arr[left]))
+    if abs(arr[right]) - abs(arr[left]) >= 0:
         right -= 1
     else:
         left += 1
-
-    ans = min(ans, abs(arr[right] + arr[left]))
 
 print(ans)
