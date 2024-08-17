ccfc8d413500e03036604456cf4d6c01ab0f6777
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-08-17 21:00:39 +0900

    Title: 정수 두 개의 합 2, Time: 197ms, Memory: 27MB, Status: Passed - CodeTree

--- 240817/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 240817/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -1,20 +1,21 @@
 def count_pairs_with_sum_less_than_k(arr, k):
     arr.sort()
-    left, right = 0, len(arr) - 1
-    count = 0
-    
-    while left < right:
-        if arr[left] + arr[right] <= k:
-            count += (right - left)
-            left += 1
-        else :
-            right -= 1
-    
-    return count
 
+    j = n
+    ans = 0
+    for i in range(1, n+1):
+        while j >= 1 and arr[i] + arr[j] > k:
+            j -= 1
+        
+        if j <= i:
+            break;
+        
+        ans += j - i
+
+    return ans
 
 n, k = map(int, input().split())
-arr = [int(input()) for _ in range(n)]
+arr = [0] + [int(input()) for _ in range(n)]
 
 result = count_pairs_with_sum_less_than_k(arr, k)
 print(result)
