02ef4984681857acefbbb18b51070a05a24a75d1
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-08-20 16:59:37 +0900

    Title: 선분 위의 점, Time: 307ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240820/선분 위의 점/point-on-the-line-segment.py
@@ -0,0 +1,40 @@
+n,m = map(int, input().split())
+vertexList = list(map(int, input().split()))
+
+def lower_bound(arr, target):
+    left, right = 0, n-1
+    min_idx = n
+
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] >= target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return min_idx
+
+
+def upper_bound(arr, target):
+    left, right = 0, n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return min_idx
+
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    first_idx, second_idx = lower_bound(vertexList, a), upper_bound(vertexList, b)
+    if first_idx == second_idx:
+        print(0)
+    else:
+        print(second_idx - first_idx)
