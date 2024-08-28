db2db7d554ba2d5effa6d4faa6d38717f5821938
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-08-28 11:05:33 +0900

    Title: 폭탄 떨구기, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240828/폭탄 떨구기/drop-the-bomb.py
+++ 240828/폭탄 떨구기/drop-the-bomb.py
@@ -1,37 +1,25 @@
 MAX_NUM = 10*9
 n, k = tuple(map(int, input().split()))
-dot_list = list()
-max_dot, min_dot = -1, MAX_NUM
-for _ in range(n):
-    num = int(input())
-    max_dot = max(max_dot, num)
-    min_dot = min(min_dot, num)
-    dot_list.append(num)
+dot_list = [int(input()) for _ in range(n)]
 dot_list.sort()
 
 
 def is_Possible(R):
     global n, k
     # 폭탄을 떨어 뜨릴때마다 범위 안에 있는 점들을 세준다는 개념으로
-    curX = dot_list[0]
-    bomb_cnt = 1
     cnt = 1
-    for i in range(1, n):
-        if curX + 2*R >= dot_list[i]:
-            cnt += 1
+    idx = 0
+    for i in range(n):
+        if dot_list[i] - dot_list[idx] <= 2*R:
+            continue
         else:
-            curX = dot_list[i]
-            bomb_cnt += 1
-            if bomb_cnt > k:
-                return False
             cnt += 1
+            idx = i
+    return cnt <= k
 
-    return cnt == n and bomb_cnt <= k
 
-
-max_range = max_dot - min_dot
-left, right = 0, max_range
-ans = max_range
+left, right = 0, MAX_NUM
+ans = MAX_NUM
 while left <= right:
     mid = (left + right) // 2
     if is_Possible(mid):
