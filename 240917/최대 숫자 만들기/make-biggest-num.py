6566ea96e51fd5fca18f470b2c90ddeacfa8cffd
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-09-17 14:19:48 +0900

    Title: 최대 숫자 만들기, Time: 600ms, Memory: 28MB, Status: Passed - CodeTree

--- 240917/최대 숫자 만들기/make-biggest-num.py
+++ 240917/최대 숫자 만들기/make-biggest-num.py
@@ -2,14 +2,9 @@ from functools import cmp_to_key
 
 # custom comparator 사용하기
 def compare(x, y):
-    result1 = x + y
-    result2 = y + x
-    result1 = int(result1)
-    result2 = int(result2)
-
-    if result1 > result2:
+    if str(x) + str(y) > str(y) + str(x):
         return -1
-    elif result1 < result2:
+    elif str(x) + str(y) < str(y) + str(x):
         return 1
     else:
         return 0
@@ -17,9 +12,10 @@ def compare(x, y):
 
 n = int(input())
 group = [
-    input() for _ in range(n)
+    int(input()) for _ in range(n)
 ]
 
 group.sort(key = cmp_to_key(compare))
-group = ''.join(group)
-print(group)
+
+for elem in group:
+    print(elem, end = '')
