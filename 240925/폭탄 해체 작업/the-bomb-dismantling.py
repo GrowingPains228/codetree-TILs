e6084f40679686a5c62960038f5c375b6bd1379e
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2024-09-25 09:45:12 +0900

    Title: 폭탄 해체 작업, Time: 423ms, Memory: 28MB, Status: Passed - CodeTree

--- 240925/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240925/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,26 +1,23 @@
 import heapq
+
 n = int(input())
-MAX_T = 0
-
-bumbList = []
+bombs = list()
+MAX_TIME = 0
 for _ in range(n):
-    (score, time_limit) = tuple(map(int, input().split()))
-    MAX_T = max(MAX_T, time_limit)
-    bumbList.append((time_limit, score))
+    score, time = tuple(map(int, input().split()))
+    MAX_TIME = max(MAX_TIME, time)
+    bombs.append((time, score))
 
-
-bumbList.sort()
-
+bombs.sort()
 pq = []
-bomb_idx = n - 1
 ans = 0
-
-for t in range(MAX_T, 0, -1):
-    while bomb_idx >= 0 and bumbList[bomb_idx][0] >= t:
-        heapq.heappush(pq, -bumbList[bomb_idx][1])
+bomb_idx = n-1
+for t in range(MAX_TIME, 0, -1):
+    while bomb_idx >= 0 and bombs[bomb_idx][0] >= t:
+        heapq.heappush(pq, -bombs[bomb_idx][1])
         bomb_idx -= 1
-    
+
     if pq:
-        ans += - heapq.heappop(pq)
+        ans += -heapq.heappop(pq)
 
 print(ans)
